59a29cf191130ea1135aa5f5680ad960455fb2bf
Sven Neumann <s.neumann@raumfeld.com>
2009-07-21 17:18:59 +0200

    raumfeld: add script for /dev fixup to devel targets

--- raumfeld/postbuild-devel-arm.sh
+++ raumfeld/postbuild-devel-arm.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
 # post-build.sh for the devel-arm target
+
+raumfeld/dev-fixup.sh $1 arm
--- raumfeld/postbuild-devel-geode.sh
+++ raumfeld/postbuild-devel-geode.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #
 # post-build.sh for the devel-geode target
+
+raumfeld/dev-fixup.sh $1 geode
