b6c60c4edada23081647f58c8159210bb5cb86b0
Sven Neumann <s.neumann@raumfeld.com>
2009-07-21 17:55:26 +0200

    raumfeld: add files to copy into the arm rootfs

--- raumfeld/postbuild-devel-arm.sh
+++ raumfeld/postbuild-devel-arm.sh
@@ -3,3 +3,10 @@
 # post-build.sh for the devel-arm target
 
 raumfeld/dev-fixup.sh $1 arm
+
+echo "Populating the root filesystem ..."
+rm -f $1/etc/resolv.conf
+cp -r raumfeld/rootfs-arm/* $1
+
+echo "Changing ownership of the root filesystem to root.root ..."
+sudo chown -R root.root $1
