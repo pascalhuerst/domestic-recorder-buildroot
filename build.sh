8ed43730c66b800d141a069744580a1a3d9dc345
Daniel Mack <daniel@caiaq.de>
2009-07-25 12:41:41 +0200

    raumfeld: build.sh tweaks
    - run with -e set (bail out on errors)
    - add remotecontrol-arm target
    - flush the build_arm dir before building
    - invoke resize2fs as post processor of imgrootfs
    - invoke raumfeld/imgcreate.sh as postprocessor of the remotecontrol-arm
      target
    
    Signed-off-by: Daniel Mack <daniel@caiaq.de>

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 case $1 in
 	# those two are pretty straight-forward
 	devel-arm)
@@ -11,12 +13,41 @@ case $1 in
 	imgrootfs-arm)
 		cp raumfeld/br2-imgrootfs-arm.config .config
 		;;
+	remotecontrol-arm)
+		cp raumfeld/br2-remotecontrol-arm.config .config
+		;;
 
 	default)
 		echo "unknown target '$1'. bummer."
 		exit -1
 esac
 
+# is that really needed?
+rm -fr build_arm
+
 make oldconfig
 make
+
+
+# do post-processing for some targets ...
+
+case $1 in
+	# resize the root fs ext2 image so that genext2fs will find
+	# free inodes when building the deployment targets.
+	# this should probably be made part of br2 some day.
+	imgrootfs-arm)
+		/sbin/resize2fs binaries/uclibc/imgrootfs.arm.ext2 500M
+		;;
+	imgrootfs-geode)
+		/sbin/resize2fs binaries/uclibc/imgrootfs.geode.ext2 500M
+		;;
+
+	remotecontrol-arm)
+		for t in flash init final; do
+			raumfeld/imgcreate.sh $1-$t arm \
+				binaries/uclibc/imgrootfs.arm.ext2 \
+				binaries/uclibc/rootfs-remotecontrol.arm.ext2
+		done
+		;;
+esac
 
