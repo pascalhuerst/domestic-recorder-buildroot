a56c98da2a882d440c4b55f5bd33f115ccd24865
Daniel Mack <daniel@caiaq.de>
2009-07-29 15:08:05 +0200

    raumfeld: add imgrootfs init.sh
    Signed-off-by: Daniel Mack <daniel@caiaq.de>

---
+++ raumfeld/testsuite/rootfs/init.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+mkdir -p /proc
+mkdir -p /sys
+mount proc /proc -t proc
+mount sys /sys -t sysfs
+mount devpts /dev/pts -t devpts
+export PATH="/sbin:/usr/sbin:$PATH"
+udevd --daemon
+
+modprobe eeti_ts flip_y=1
+modprobe pxamci
+modprobe libertas_sdio
+modprobe wire.ko delay_coef=3
+modprobe w1-gpio.ko
+modprobe w1_ds2760.ko
+
+
+############################################################
+WIRELESS_DEV=eth0
+iwconfig $WIRELESS_DEV essid bugwerft
+iwconfig $WIRELESS_DEV rate 54M
+wpa_supplicant -Dwext -i$WIRELESS_DEV -c/etc/wpa_supplicant.conf -B
+ifconfig $WIRELESS_DEV 192.168.1.56
+############################################################
+
+rm -fr /var/empty
+mkdir -p /var/empty
+chmod 755 /var/empty
+/etc/init.d/S50sshd start
+
+export TERM=xterm-color
+exec /bin/sh
+/start-test.sh
+
