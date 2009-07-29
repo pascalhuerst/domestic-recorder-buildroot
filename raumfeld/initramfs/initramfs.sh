209c1c11445789e6362436420b4e372f6382742d
Daniel Mack <daniel@caiaq.de>
2009-07-29 17:19:44 +0200

    raumfeld: remotecontrol: deploy as tgz
    Signed-off-by: Daniel Mack <daniel@caiaq.de>

--- raumfeld/initramfs/initramfs.sh
+++ raumfeld/initramfs/initramfs.sh
@@ -3,6 +3,22 @@
 export PATH="/sbin:/usr/sbin:$PATH"
 
 echo "Booted to initramfs. Now switching over to imgrootfs."
+
+hw=`cat /proc/cpuinfo | grep ^Hardware | cut -f 3 -d' '`
+
+case "$hw" in
+	# FIXME!!!
+	proto)
+		img="uImage"
+		;;
+
+	*)
+		img="uImage"
+		echo "unknown hardware type '$hw'"
+		;;
+esac
+
+echo "Image name $img"
 echo "Waiting for USB device to appear ..."
 
 while [ -z "$(grep sda1 /proc/partitions)" ]; do
@@ -11,7 +27,7 @@ done
 
 mkdir /usb
 mount /dev/sda1 /usb
-losetup -o 5128192 /dev/loop0 /usb/uImage
+losetup -o 5128192 /dev/loop0 /usb/$img
 
 mkdir /rootfs
 mount -t ext2 /dev/loop0 /rootfs
