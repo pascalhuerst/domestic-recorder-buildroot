7a70ef8c87f80ab45778256615cd56961bd46996
Sven Neumann <s.neumann@raumfeld.com>
2009-08-06 17:21:55 +0200

    raumfeld: add new package sly-toolkit

---
+++ package/sly-toolkit/sly-toolkit.mk
@@ -0,0 +1,38 @@
+#############################################################
+#
+# sly-toolkit
+#
+#############################################################
+
+SLY_TOOLKIT_VERSION = $(BR2_PACKAGE_SLY_TOOLKIT_BRANCH)
+SLY_TOOLKIT_AUTORECONF = YES
+SLY_TOOLKIT_LIBTOOL_PATCH = NO
+SLY_TOOLKIT_INSTALL_STAGING = YES
+SLY_TOOLKIT_INSTALL_TARGET = YES
+
+SLY_TOOLKIT_CONF_ENV = \
+	ac_cv_path_GLIB_GENMARSHAL=$(HOST_GLIB)/bin/glib-genmarshal
+
+SLY_TOOLKIT_CONF_OPT = \
+	--enable-shared		\
+	--enable-static		\
+	--disable-explicit-deps \
+	--disable-glibtest	\
+	--disable-gtk-doc --without-html-dir
+
+SLY_TOOLKIT_DEPENDENCIES = uclibc host-pkgconfig libglib2 directfb
+
+$(eval $(call AUTOTARGETS,package,sly-toolkit))
+
+$(SLY_TOOLKIT_DIR)/.bzr:
+	if [ ! -a $(SLY_TOOLKIT_DIR)/.bzr ]; then \
+	  	(cd $(BUILD_DIR); \
+	 	$(BZR_CO) $(BR2_PACKAGE_RAUMFELD_REPOSITORY)/sly-toolkit/$(SLY_TOOLKIT_VERSION) sly-toolkit-$(SLY_TOOLKIT_VERSION)) \
+	fi
+
+$(SLY_TOOLKIT_DIR)/.stamp_downloaded: $(SLY_TOOLKIT_DIR)/.bzr
+	touch $@
+
+$(SLY_TOOLKIT_DIR)/.stamp_extracted: $(SLY_TOOLKIT_DIR)/.stamp_downloaded
+	(cd $(SLY_TOOLKIT_DIR); gtkdocize)
+	touch $@
