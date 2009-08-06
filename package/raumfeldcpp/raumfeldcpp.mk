70d5f638bfcfd942fa061c2f3aa53141bd95f4e8
Sven Neumann <s.neumann@raumfeld.com>
2009-08-06 17:15:47 +0200

    raumfeld: add new package raumfeldcpp

---
+++ package/raumfeldcpp/raumfeldcpp.mk
@@ -0,0 +1,33 @@
+#############################################################
+#
+# raumfeldcpp
+#
+#############################################################
+
+RAUMFELDCPP_VERSION = $(BR2_PACKAGE_RAUMFELDCPP_BRANCH)
+RAUMFELDCPP_AUTORECONF = YES
+RAUMFELDCPP_LIBTOOL_PATCH = NO
+RAUMFELDCPP_INSTALL_STAGING = YES
+RAUMFELDCPP_INSTALL_TARGET = YES
+
+RAUMFELDCPP_CONF_OPT = \
+	--enable-shared		\
+	--enable-static		\
+	--disable-explicit-deps \
+	--disable-glibtest
+
+RAUMFELDCPP_DEPENDENCIES = uclibc libsoup
+
+$(eval $(call AUTOTARGETS,package,raumfeldcpp))
+
+$(RAUMFELDCPP_DIR)/.bzr:
+	if [ ! -a $(RAUMFELDCPP_DIR)/.bzr ]; then \
+	  	(cd $(BUILD_DIR); \
+	 	$(BZR_CO) $(BR2_PACKAGE_RAUMFELD_REPOSITORY)/raumfeldcpp/$(RAUMFELDCPP_VERSION) raumfeldcpp-$(RAUMFELDCPP_VERSION)) \
+	fi
+
+$(RAUMFELDCPP_DIR)/.stamp_downloaded: $(RAUMFELDCPP_DIR)/.bzr
+	touch $@
+
+$(RAUMFELDCPP_DIR)/.stamp_extracted: $(RAUMFELDCPP_DIR)/.stamp_downloaded
+	touch $@
