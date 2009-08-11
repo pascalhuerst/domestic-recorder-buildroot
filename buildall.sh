dd0e1c68a190d90e0d5c2a4e1662e211b9fe347e
Sven Neumann <s.neumann@raumfeld.com>
2009-08-11 13:44:22 +0200

    raumfeld: add audioadapter-arm to the buildall.sh script

--- buildall.sh
+++ buildall.sh
@@ -2,10 +2,12 @@
 
 set -e
 
-# initramfs and imgrootfs is needed to build the other targets, so build that first
+# initramfs and imgrootfs is needed to build before the other targets,
+# so build them first
 ./build.sh initramfs-arm
 ./build.sh imgrootfs-arm
 
+./build.sh audioadapter-arm
 ./build.sh remotecontrol-arm
 # add others here ...
 
