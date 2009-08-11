7821e34eb062d00fc75a7a56e4a7526eae560a8d
Sven Neumann <s.neumann@raumfeld.com>
2009-08-11 15:58:44 +0200

    raumfeld: generate SSH keys for the imgrootfs

--- raumfeld/postbuild-imgrootfs-arm.sh
+++ raumfeld/postbuild-imgrootfs-arm.sh
@@ -6,6 +6,15 @@ echo "Populating the root filesystem ..."
 
 cp -av raumfeld/testsuite/rootfs/* $1/
 
+
+echo "Creating SSH host keys..."
+
+ssh-keygen -q -t dsa -N "" -C "Raumfeld factory tests" -f $1/etc/ssh_host_dsa
+ssh-keygen -q -t rsa -N "" -C "Raumfeld factory tests" -f $1/etc/ssh_host_rsa
+
+
+echo "Building and installing test binaries..."
+
 GCC=build_arm/staging_dir/usr/bin/arm-linux-gcc
 
 $GCC -o $1/input_test -Wall raumfeld/testsuite/input_test/input_test.c
