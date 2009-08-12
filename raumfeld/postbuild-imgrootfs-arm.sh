c16fabe0793b28e3c127bc1d85fe17053e503533
Sven Neumann <s.neumann@raumfeld.com>
2009-08-12 16:19:07 +0200

    raumfeld: remove the ssh-keygen machinery for now, it's broken

--- raumfeld/postbuild-imgrootfs-arm.sh
+++ raumfeld/postbuild-imgrootfs-arm.sh
@@ -8,14 +8,6 @@ cp -av raumfeld/testsuite/rootfs/* $1/
 cp -av raumfeld/rootfs-arm/lib $1
 
 
-echo "Creating SSH host keys..."
-
-SSH_KEYGEN="ssh-keygen -q -N \"\" -C \"Raumfeld factory tests\"" 
-
-$SSH_KEYGEN -t dsa -f $1/etc/ssh_host_dsa_key
-$SSH_KEYGEN -t rsa -f $1/etc/ssh_host_rsa_key
-
-
 echo "Building and installing test binaries..."
 
 GCC=build_arm/staging_dir/usr/bin/arm-linux-gcc
