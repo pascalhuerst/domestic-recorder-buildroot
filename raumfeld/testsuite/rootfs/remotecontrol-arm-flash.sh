6fcb10f6dc64e5fdd77287c504a5567462299146
Sven Neumann <s.neumann@raumfeld.com>
2009-08-25 11:20:34 +0200

    raumfeld: feedback while flashing on rescue boot
    Add rescue boot logo (black on white) and change the
    remotecontrol-arm-flash.sh script to use progress_fb.

--- raumfeld/testsuite/rootfs/remotecontrol-arm-flash.sh
+++ raumfeld/testsuite/rootfs/remotecontrol-arm-flash.sh
@@ -2,6 +2,18 @@
 
 source tests.inc
 
-cd tests
+TITLE="NANDFLASH"
+DIALOGOPTS="--title $TITLE"
 
-./nand && reboot
+BASE="/raumfeld-logo.raw"
+COLOR="0"
+
+(tests/init_flash || dialog_err "ERROR!" $DIALOGOPTS; exit 1) | \
+    /percent 2023 | \
+    /progress_fb $BASE 0 0 20 136 $COLOR
+
+(tests/copy_rootfs || dialog_err "ERROR!" $DIALOGOPTS; exit 1) | \
+    /percent `cat /rootfs.tgz.numfiles` | \
+    /progress_fb $BASE 0 136 20 136 $COLOR
+
+reboot
