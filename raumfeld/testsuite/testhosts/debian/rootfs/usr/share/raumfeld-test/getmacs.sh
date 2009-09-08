049f84c7ba6cc4e1a1022bd144a1a882fb896fa6
Daniel Mack <daniel@caiaq.de>
2009-09-08 13:47:36 +0800

    raumfeld: testhost: suck macs
    Signed-off-by: Daniel Mack <daniel@caiaq.de>

---
+++ raumfeld/testsuite/testhosts/debian/rootfs/usr/share/raumfeld-test/getmacs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+nmacs=100
+list=/var/raumfeld-test/macaddr.list
+tmp=/tmp/getmacs.tmp
+# 00:26:06 -> 9734
+pool=9734
+server=http://buildcontrol.caiaq.de/macaddr.php
+
+curr=$(cat $list | wc -l)
+get=$(($nmacs - $curr))
+
+wget "$server?machine=$host&pool=$pool&num=$get" -O $tmp
+cat $tmp >> $list
+rm -f $tmp
+
+
