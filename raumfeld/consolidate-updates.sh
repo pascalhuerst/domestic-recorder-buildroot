786f135b99ad20b235016b3446ebe7f3c0991814
Sven Neumann <s.neumann@raumfeld.com>
2009-09-24 12:42:56 +0200

    raumfeld: fix bug introduced with my last commit

--- raumfeld/consolidate-updates.sh
+++ raumfeld/consolidate-updates.sh
@@ -2,7 +2,7 @@
 
 update_ssh="devel.internal:/var/www/devel/updates"
 
-if test -n ${UPDATE_SSH}; then
+if test -n "${UPDATE_SSH}"; then
   update_ssh=${UPDATE_SSH}
 fi
 
@@ -16,6 +16,8 @@ mkdir www
 for u in $updates; do
 	cp $u/* www/
 done
+
+echo "Uploading to $update_ssh"
 
 rsync -ravv -e ssh www/* $update_ssh
 
