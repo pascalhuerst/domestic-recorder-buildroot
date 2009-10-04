6c459c1287d9c18286a72fdbdf825dde9d0bb076
Sven Neumann <s.neumann@raumfeld.com>
2009-10-04 17:19:50 +0200

    taglib: revert build changes
    The changes that I did in an attempt to get taglib to build on the
    buildbot slave did not work out. Revert so that the builds on our
    development PCs work again.

--- package/multimedia/taglib/taglib.mk
+++ package/multimedia/taglib/taglib.mk
@@ -6,7 +6,6 @@
 TAGLIB_VERSION = 1.5
 TAGLIB_SOURCE = taglib-$(TAGLIB_VERSION).tar.gz
 TAGLIB_SITE = http://developer.kde.org/~wheeler/files/src
-TAGLIB_AUTORECONF = YES
 TAGLIB_LIBTOOL_PATCH = NO
 TAGLIB_INSTALL_STAGING = YES
 
@@ -21,7 +20,7 @@ TAGLIB_CONF_ENV = \
 	ac_cv_header_cppunit_extensions_HelperMacros_h=no \
 	ac_cv_header_zlib_h=$(if $(BR2_PACKAGE_ZLIB),yes,no)
 
-TAGLIB_CONF_OPT = --disable-fast-install --disable-libsuffix --program-prefix=''
+TAGLIB_CONF_OPT = --disable-libsuffix --program-prefix=''
 
 $(eval $(call AUTOTARGETS,package/multimedia,taglib))
 
