2d9c5466cb1f6b850efeac97c837b1b7acb764d4
Daniel Mack <daniel@caiaq.de>
2009-10-09 16:25:26 +0800

    raumfeld: more test script updates

--- raumfeld/testsuite/rootfs/remotecontrol-arm-final.sh
+++ raumfeld/testsuite/rootfs/remotecontrol-arm-final.sh
@@ -4,22 +4,14 @@ cd /tests
 
 . ../tests.inc
 
-# different behaviour in case the test was started via ssh
-if [ "$1" = "ssh" ]; then
-	./framebuffer &&	\
-	./rotary &&		\
-	./accel-full &&		\
-	./touch &&		\
-	./battery
-
-	test_result
-
-	exit 0
-fi
-
-./wifi-connect
-
-dialog_msg "You shouldn't see this message. \
+dialog_info "You shouldn't see this message. \
 This image is made for final assembly test with no serial \
-adapter connected. This image will only wait for remote ssh connection"
+adapter connected."
+
+TERM=xterm-color ./wifi		>/dev/tty1 && \
+TERM=xterm-color ./rotary	>/dev/tty1 && \
+TERM=xterm-color ./accel-full	>/dev/tty1 && \
+TERM=xterm-color ./battery dock	>/dev/tty1 && \
+./touch	&& \
+(cat hellokitty.raw > /dev/fb0)
 
