fc740c42c091724d4529c7f9c906d586a7c1f4c1
Sven Neumann <s.neumann@raumfeld.com>
2009-10-09 19:52:55 +0200

    raumfeld: fix gcc name

--- raumfeld/postbuild-imgrootfs-geode.sh
+++ raumfeld/postbuild-imgrootfs-geode.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-# post-build.sh for the imgrootfs-arm target
+# post-build.sh for the imgrootfs-geode target
 
 echo "Building and installing test binaries..."
 
-GCC=build_i586/staging_dir/usr/bin/i586-linux-uclibcgnueabi-gcc
+GCC=build_i586/staging_dir/usr/bin/i586-linux-uclibc-gcc
 
 $GCC -o $1/progress_fb -Wall raumfeld/testsuite/progress_fb/progress_fb.c
 $GCC -o $1/input_test -Wall raumfeld/testsuite/input_test/input_test.c
