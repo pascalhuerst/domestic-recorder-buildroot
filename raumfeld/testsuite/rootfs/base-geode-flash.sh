c65424b6408826cc2675d7bc2e5c99c3a2cad58a
Daniel Mack <daniel@caiaq.de>
2009-10-12 16:19:05 +0800

    raumfeld: geode related script updates

--- raumfeld/testsuite/rootfs/base-geode-flash.sh
+++ raumfeld/testsuite/rootfs/base-geode-flash.sh
@@ -4,6 +4,15 @@ source tests.inc
 
 cd tests
 
-./hda1 &&	\
-dialog_msg "ALL TESTS PASSED."
+./leds-blink 1 &
+pid=$!
+
+./harddisk || touch /tmp/test-failed
+
+if [ -f /tmp/test-failed ]; then
+        kill $pid
+        ./leds-blink 2
+fi
+
+reboot
 
