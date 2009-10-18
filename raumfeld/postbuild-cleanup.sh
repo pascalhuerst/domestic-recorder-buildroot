f7ae0095b8a64d95c66e0941853d8e7b1eb3fabf
Sven Neumann <s.neumann@raumfeld.com>
2009-10-18 23:49:53 +0200

    raumfeld: remove unneeded files
    Remove some files from the target filesystem that we will
    definitely not need.

--- raumfeld/postbuild-cleanup.sh
+++ raumfeld/postbuild-cleanup.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 rm -fr $1/usr/bin/libarchive*
-
+rm -fr $1/usr/include
+rm -fr $1/usr/lib/pkg-config
+rm -f  $1/usr/lib/*.la
+rm -f  $1/usr/libexec/gvfsd-burn
+rm -f  $1/usr/libexec/gvfsd-trash
+rm -fr $1/usr/share/aclocal
+rm -fr $1/usr/share/gdb
+rm -fr $1/usr/share/glib-2.0
