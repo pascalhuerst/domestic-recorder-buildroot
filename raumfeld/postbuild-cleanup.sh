f2c9136bdc8ddc8dbf2d469288d38995978aa0a2
Sven Neumann <s.neumann@raumfeld.com>
2009-10-19 21:30:43 +0200

    raumfeld: remove some more unneeded files

--- raumfeld/postbuild-cleanup.sh
+++ raumfeld/postbuild-cleanup.sh
@@ -3,6 +3,18 @@
 # post-build cleanup for the target rootfs
 
 rm -fr $1/usr/include
+rm -f  $1/usr/bin/glib-genmarshal
+rm -f  $1/usr/bin/glib-gettextize
+rm -f  $1/usr/bin/glib-mkenums
+rm -f  $1/usr/bin/gobject-query
+rm -f  $1/usr/bin/gtester
+rm -f  $1/usr/bin/gtester-report
+rm -f  $1/usr/bin/gupnp-binding-tool
+rm -f  $1/usr/bin/iconv
+rm -f  $1/usr/bin/ssh-keyscan
+rm -f  $1/usr/bin/xml2-config
+rm -f  $1/usr/bin/xmlcatalog
+rm -f  $1/usr/bin/xmllint
 rm -fr $1/usr/lib/pkg-config
 rm -f  $1/usr/lib/*.la
 rm -fr $1/usr/lib/libarchive*
