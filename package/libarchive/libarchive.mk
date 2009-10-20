a50d07ba2aabb13ef3fbf56c6eac0e9a82bdef08
Sven Neumann <s.neumann@raumfeld.com>
2009-10-20 21:32:14 +0200

    libarchive: make bsdtar and bsdcpio binaries configurable
    libarchive comes with two binaries, bsdtar and bsdcpio that used
    to be installed by default. This change makes this configurable
    with the default not to build and install the binaries.

--- package/libarchive/libarchive.mk
+++ package/libarchive/libarchive.mk
@@ -12,4 +12,8 @@ LIBARCHIVE_INSTALL_TARGET = YES
 
 LIBARCHIVE_DEPENDENCIES = uclibc
 
+LIBARCHIVE_CONF_OPT = \
+	$(if $(BR2_PACKAGE_LIBARCHIVE_BSDTAR),--enable-bsdtar,--disable-bsdtar) \
+	$(if $(BR2_PACKAGE_LIBARCHIVE_BSDCPIO),--enable-bsdcpio,--disable-bsdcpio)
+
 $(eval $(call AUTOTARGETS,package,libarchive))
