a6afeb43dd3544018e8f546c01256a712c4e478b
Henry Hoegelow <h.hoegelow@raumfeld.com>
2009-11-11 12:06:22 +0100

    remote-control: remote is now logging through logger inti /var/log/messages

--- raumfeld/rootfs-remotecontrol-arm/usr/bin/remote-control.sh
+++ raumfeld/rootfs-remotecontrol-arm/usr/bin/remote-control.sh
@@ -3,6 +3,18 @@
 export RAUMFELD_UPDATES_URL=http://devel.internal/updates/
 
 while (true); do
-  remote-control > /var/log/remote-control.log 2>&1
-  sleep 1
+
+    echo "creating pipe"
+    mknod /tmp/master-process-logger-pipe pipe
+
+    echo "starting logger"
+    logger -t "remote-control" < /tmp/master-process-logger-pipe &
+
+    echo "starting remote-control"
+    remote-control > /tmp/master-process-logger-pipe 2>&1
+    
+    echo "remote-control exited"
+
+    sleep 1
+
 done
