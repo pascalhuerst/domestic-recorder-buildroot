4381b61e9ee6075f4374612fc0caf2de657e8930
Daniel Mack <daniel@caiaq.de>
2010-02-10 14:22:05 +0100

    raumfeld: flash-uboot: also write default env
    .. and save the ethaddr if already set

--- raumfeld/testsuite/rootfs/flash-uboot.sh
+++ raumfeld/testsuite/rootfs/flash-uboot.sh
@@ -24,5 +24,11 @@ case "$hw" in
 		exit 0
 esac
 
-cat /$img > /dev/mtdblock0
+# write the bootloader
+dd bs=1024 count=640 if=/$img of=/dev/mtdblock0
+
+# reset the environment, save ethaddr if already set
+eval $(fw_printenv ethaddr)
+dd bs=1024 count=128 skip=640 if=/$img of=/dev/mtdblock1
+test -z "$ethaddr" || fw_setenv ethaddr $ethaddr
 
