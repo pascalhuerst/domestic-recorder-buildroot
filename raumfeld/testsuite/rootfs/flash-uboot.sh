81740907eeaac3721bafd6f84c69464f3d469ed1
Daniel Mack <daniel@caiaq.de>
2010-02-10 15:22:10 +0100

    raumfeld: keep u-boot images for each hw revision

--- raumfeld/testsuite/rootfs/flash-uboot.sh
+++ raumfeld/testsuite/rootfs/flash-uboot.sh
@@ -1,29 +1,27 @@
 #!/bin/sh
 
-hw=`cat /proc/cpuinfo | grep ^Hardware | cut -f 3 -d' '`
-
-if [ -z "$hw" ]; then
-	hw=`cat /proc/cpuinfo | grep ^model\ name | cut -f 3 -d' '`
-fi
+hw=$(cat /proc/cpuinfo | grep ^Hardware | cut -f 3 -d' ')
+rev=$(cat /proc/cpuinfo | grep ^Revision | cut -f 2 -d: | cut -f2 -d' ')
 
 case "$hw" in
 	Controller)
-		img="raumfeld-controller.bin"
+		img="raumfeld-controller-$rev.bin"
 		;;
 	Connector)
-		img="raumfeld-connector.bin"
+		img="raumfeld-connector-$rev.bin"
 		;;
 	Speaker)
-		if [ -z "`cat /proc/cpuinfo | grep ^Revision | grep ': 01'`" ]; then
-			img="raumfeld-speaker_s.bin"
-		else
-			img="raumfeld-speaker_m.bin"
-		fi
+		img="raumfeld-speaker-$rev.bin"
 		;;
 	*)
 		echo "Failed to parse machine data. Ups."
 		exit 0
 esac
+
+if [ ! -f /$img ]; then
+	echo "/$img does not exist. Bummer."
+	exit 0
+fi
 
 # write the bootloader
 dd bs=1024 count=640 if=/$img of=/dev/mtdblock0
@@ -32,7 +30,4 @@ dd bs=1024 count=640 if=/$img of=/dev/mtdblock0
 eval $(fw_printenv ethaddr)
 dd bs=1024 count=128 skip=640 if=/$img of=/dev/mtdblock1
 test -z "$ethaddr" || fw_setenv ethaddr $ethaddr
-
-# restore board revision information
-fw_setenv boardrev $(cat /proc/cpuinfo | grep ^Revision | cut -f 2 -d:)
 
