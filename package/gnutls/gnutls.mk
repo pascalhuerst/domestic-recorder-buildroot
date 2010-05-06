95baa99bfb49f21b01fcfb15d0186397ef7dc2e0
Sven Neumann <s.neumann@raumfeld.com>
2010-05-06 22:00:59 +0200

    gnutls: bump to version 2.8.6 and point to libgcrypt installation
    Fix picked from upstream.

--- package/gnutls/gnutls.mk
+++ package/gnutls/gnutls.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-GNUTLS_VERSION:=2.8.5
+GNUTLS_VERSION:=2.8.6
 GNUTLS_SOURCE:=gnutls-$(GNUTLS_VERSION).tar.bz2
 GNUTLS_SITE:=ftp://ftp.gnutls.org/pub/gnutls
 GNUTLS_AUTORECONF = NO
@@ -13,5 +13,8 @@ GNUTLS_INSTALL_STAGING = YES
 GNUTLS_INSTALL_TARGET = YES
 
 GNUTLS_DEPENDENCIES = host-pkgconfig libglib2 libgcrypt
+
+GNUTLS_CONF_OPT = \
+	--with-libgcrypt-prefix=$(STAGING_DIR)/usr
 
 $(eval $(call AUTOTARGETS,package,gnutls))
