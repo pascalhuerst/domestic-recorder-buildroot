51efd2e28ec68165b72dbb905c3e5c0f3d06cfb7
Sven Neumann <s.neumann@raumfeld.com>
2010-05-06 11:10:13 +0200

    shared-mime-info: run update-mime-database with correct env variables
    Fix picked from upstream.

--- package/shared-mime-info/shared-mime-info.mk
+++ package/shared-mime-info/shared-mime-info.mk
@@ -67,5 +67,5 @@ host-shared-mime-info-dirclean:
 
 # update the shared-mime-info database in the target
 $(SHARED_MIME_INFO_HOOK_POST_INSTALL): host-shared-mime-info
-	$(SHARED_MIME_INFO_HOST_BINARY) $(STAGING_DIR)/usr/share/mime
+	$(HOST_MAKE_ENV) $(SHARED_MIME_INFO_HOST_BINARY) $(STAGING_DIR)/usr/share/mime
 	$(INSTALL) -D $(STAGING_DIR)/usr/share/mime/mime.cache $(TARGET_DIR)/usr/share/mime/mime.cache
