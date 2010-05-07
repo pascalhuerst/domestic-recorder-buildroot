8707372d479e9ad1a88722b334a7c53635293aa3
Sven Neumann <s.neumann@raumfeld.com>
2010-05-07 09:21:33 +0200

    libgcrypt: download from official site

--- package/libgcrypt/libgcrypt.mk
+++ package/libgcrypt/libgcrypt.mk
@@ -6,7 +6,7 @@
 
 LIBGCRYPT_VERSION:=1.4.5
 LIBGCRYPT_SOURCE:=libgcrypt-$(LIBGCRYPT_VERSION).tar.bz2
-LIBGCRYPT_SITE:=ftp://ftp.freenet.de/pub/ftp.gnupg.org/gcrypt/libgcrypt/
+LIBGCRYPT_SITE:=ftp://ftp.gnupg.org/gcrypt/gnupg
 LIBGCRYPT_INSTALL_STAGING:=YES
 
 LIBGCRYPT_CONF_OPT = --disable-asm
