91009cc023cbf899ebaedc5b444c64e3b9bd1082
Sven Neumann <s.neumann@raumfeld.com>
2010-05-10 09:50:03 +0200

    p0-timeserver: check out trunk

--- package/raumfeld/p0-timeserver/p0-timeserver.mk
+++ package/raumfeld/p0-timeserver/p0-timeserver.mk
@@ -26,7 +26,7 @@ $(P0_TIMESERVER_DIR)/.bzr:
 	if ! test -d $(P0_TIMESERVER_DIR)/.bzr; then \
 	  	(cd $(BUILD_DIR); \
 		mkdir -p p0-timeserver-$(P0_TIMESERVER_VERSION); \
-	 	$(BZR_CO) $(BR2_PACKAGE_RAUMFELD_REPOSITORY)/p0-timeserver p0-timeserver-$(P0_TIMESERVER_VERSION)) \
+	 	$(BZR_CO) $(BR2_PACKAGE_RAUMFELD_REPOSITORY)/p0-timeserver/$(P0_TIMESERVER_VERSION) p0-timeserver-$(P0_TIMESERVER_VERSION)) \
 	fi
 	touch -c $@
 
