229a3691e51d6dd38f9517ba09c93fdaf0e06fbb
Sven Neumann <s.neumann@raumfeld.com>
2010-05-16 22:08:57 +0200

    gst-ffmpeg: update to version 0.10.10
    Signed-off-by: Sven Neumann <s.neumann@raumfeld.com>

--- package/multimedia/gst-ffmpeg/gst-ffmpeg.mk
+++ package/multimedia/gst-ffmpeg/gst-ffmpeg.mk
@@ -3,7 +3,7 @@
 # gst-ffmpeg
 #
 #############################################################
-GST_FFMPEG_VERSION = 0.10.9
+GST_FFMPEG_VERSION = 0.10.10
 GST_FFMPEG_SOURCE = gst-ffmpeg-$(GST_FFMPEG_VERSION).tar.bz2
 GST_FFMPEG_SITE = http://gstreamer.freedesktop.org/src/gst-ffmpeg
 GST_FFMPEG_INSTALL_STAGING = YES
