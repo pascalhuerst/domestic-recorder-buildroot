b68967faaf92ebb811532d8a3f41542987aaa977
Sven Neumann <s.neumann@raumfeld.com>
2010-05-31 16:33:33 +0200

    iso-codes: add iso-codes package
    Signed-off-by: Sven Neumann <s.neumann@raumfeld.com>

---
+++ package/iso-codes/iso-codes.mk
@@ -0,0 +1,13 @@
+#############################################################
+#
+# iso-codes
+#
+#############################################################
+ISO_CODES_VERSION:=3.16
+ISO_CODES_SOURCE:=iso-codes-$(ISO_CODES_VERSION).tar.bz2
+ISO_CODES_SITE:=ftp://pkg-isocodes.alioth.debian.org/pub/pkg-isocodes/
+ISO_CODES_INSTALL_STAGING = YES
+ISO_CODES_INSTALL_TARGET = YES
+ISO_CODES_DEPENDENCIES = gettext
+
+$(eval $(call AUTOTARGETS,package,iso-codes))
