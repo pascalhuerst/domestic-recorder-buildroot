bf8f8dbf69e12edd06d99a18fdd46c2fc821f567
Sven Neumann <s.neumann@raumfeld.com>
2010-07-22 23:08:36 +0200

    libunwind: use a git snapshot and configure with --enable-debug-frame
    Signed-off-by: Sven Neumann <s.neumann@raumfeld.com>

--- package/libunwind/libunwind.mk
+++ package/libunwind/libunwind.mk
@@ -4,12 +4,19 @@
 #
 #############################################################
 
-LIBUNWIND_VERSION:=0.99
+LIBUNWIND_VERSION:=99e60be
 LIBUNWIND_SOURCE:=libunwind-$(LIBUNWIND_VERSION).tar.gz
-LIBUNWIND_SITE:=http://download.savannah.nongnu.org/releases/libunwind/
-LIBUNWIND_AUTORECONF = NO
+LIBUNWIND_URL:=http://git.savannah.gnu.org/gitweb/?p=libunwind.git;a=snapshot;h=$(LIBUNWIND_VERSION);sf=tgz
+LIBUNWIND_AUTORECONF = YES
 LIBUNWIND_LIBTOOL_PATCH = NO
 LIBUNWIND_INSTALL_STAGING = YES
 LIBUNWIND_INSTALL_TARGET = YES
 
+LIBUNWIND_CONF_OPT = --enable-debug-frame
+
 $(eval $(call AUTOTARGETS,package,libunwind))
+
+$(LIBUNWIND_DIR)/.stamp_downloaded:
+	$(WGET) -O $(DL_DIR)/$(LIBUNWIND_SOURCE) "$(LIBUNWIND_URL)"
+	$(Q)mkdir -p $(LIBUNWIND_DIR)
+	$(Q)touch $@
