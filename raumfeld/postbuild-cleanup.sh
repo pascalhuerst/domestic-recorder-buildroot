e1d0ee82ca6b94955e96d5fb5824a39b6f31d75f
Sven Neumann <s.neumann@raumfeld.com>
2010-08-04 20:27:38 +0200

    raumfeld: strip libsmbclient

--- raumfeld/postbuild-cleanup.sh
+++ raumfeld/postbuild-cleanup.sh
@@ -70,6 +70,7 @@ if test -n "$2"; then
     find $1/usr/sbin -type f -executable -exec $STRIPCMD {} \;
     find $1/usr/libexec -type f -executable -exec $STRIPCMD {} \;
     find $1/usr/lib -type f -executable -name 'libgst*' -exec $STRIPCMD {} \;
+    find $1/usr/lib -type f -executable -name 'libsmb*' -exec $STRIPCMD {} \;
     find $1/usr/lib/udev -type f -executable -exec $STRIPCMD {} \;
     if test -d $1/usr/lib/alsa-lib; then
         find $1/usr/lib/alsa-lib -type f -executable -exec $STRIPCMD {} \;
