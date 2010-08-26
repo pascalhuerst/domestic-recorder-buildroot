3977c0b7e86113ba4a8bf31f97d693d518072c43
Henry Hoegelow <h.hoegelow@raumfeld.com>
2010-08-26 14:52:19 +0200

    gst-ffmpeg: disabled h264 support as it irritates the type find for some mp3

--- package/multimedia/gst-ffmpeg/gst-ffmpeg.mk
+++ package/multimedia/gst-ffmpeg/gst-ffmpeg.mk
@@ -43,7 +43,8 @@ GST_FFMPEG_CONF_OPT = \
 						--enable-cross-compile \
 						--cross-prefix=$(TARGET_CROSS) \
 						--disable-shared \
-						--enable-static"
+						--enable-static \
+            --disable-decoder=h264"
 
 $(DL_DIR)/$(GST_FFMPEG_SOURCE):
 	$(call DOWNLOAD,$(GST_FFMPEG_SITE),$(GST_FFMPEG_SOURCE))
