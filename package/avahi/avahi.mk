da4f88b480d8637de362e6f99562256f7cf17a49
Sven Neumann <s.neumann@raumfeld.com>
2010-10-27 17:28:51 +0200

    avahi: update to version 0.6.25
    Signed-off-by: Sven Neumann <s.neumann@raumfeld.com>

--- package/avahi/avahi.mk
+++ package/avahi/avahi.mk
@@ -10,9 +10,10 @@
 # either version 2.1 of the License, or (at your option) any
 # later version.
 
-AVAHI_VERSION = 0.6.23
+AVAHI_VERSION = 0.6.25
 AVAHI_SOURCE = avahi-$(AVAHI_VERSION).tar.gz
 AVAHI_SITE = http://www.avahi.org/download/
+AVAHI_LIBTOOL_PATCH = NO
 AVAHI_INSTALL_STAGING = YES
 AVAHI_INSTALL_TARGET = YES
 
