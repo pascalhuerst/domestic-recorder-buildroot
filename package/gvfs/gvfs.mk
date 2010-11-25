bda81d5b04c1730bdf5c55d0ab035bfc2f81c191
Sven Neumann <s.neumann@raumfeld.com>
2010-11-25 10:47:08 +0100

    gvfs: upgrade to version 1.6.5

--- package/gvfs/gvfs.mk
+++ package/gvfs/gvfs.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 GVFS_VERSION_MAJOR = 1.6
-GVFS_VERSION_MINOR = 3
+GVFS_VERSION_MINOR = 5
 GVFS_VERSION = $(GVFS_VERSION_MAJOR).$(GVFS_VERSION_MINOR)
 GVFS_SOURCE = gvfs-$(GVFS_VERSION).tar.gz
 GVFS_SITE = http://ftp.gnome.org/pub/GNOME/sources/gvfs/$(GVFS_VERSION_MAJOR)
