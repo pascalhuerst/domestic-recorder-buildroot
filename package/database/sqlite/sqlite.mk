d73d42f594f958ec8580b8a150147e488034fc90
Sven Neumann <s.neumann@raumfeld.com>
2011-01-21 13:47:41 +0100

    sqlite: update to version 3.7.4

--- package/database/sqlite/sqlite.mk
+++ package/database/sqlite/sqlite.mk
@@ -4,8 +4,9 @@
 #
 #############################################################
 
-SQLITE_VERSION = 3.6.23.1
-SQLITE_SOURCE = sqlite-amalgamation-$(SQLITE_VERSION).tar.gz
+SQLITE_VERSION = 3.7.4
+SQLITE_PACKAGE_VERSION = 3070400
+SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_PACKAGE_VERSION).tar.gz
 SQLITE_SITE = http://www.sqlite.org
 SQLITE_INSTALL_STAGING = YES
 SQLITE_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
