e7b6390d840faa7344586240492ba554727a933a
Sven Neumann <s.neumann@raumfeld.com>
2011-03-08 10:20:57 +0100

    avahi: add a dependency on host-pkg-config

--- package/avahi/avahi.mk
+++ package/avahi/avahi.mk
@@ -91,7 +91,7 @@ AVAHI_CONF_OPT = --localstatedir=/var \
 		--with-autoipd-user=default \
 		--with-autoipd-group=default
 
-AVAHI_DEPENDENCIES = $(if $(BR2_PACKAGE_GETTEXT),gettext)
+AVAHI_DEPENDENCIES = $(if $(BR2_PACKAGE_GETTEXT),gettext) host-pkg-config
 
 ifneq ($(BR2_PACKAGE_AVAHI_DAEMON)$(BR2_PACKAGE_AVAHI_AUTOIPD),)
 AVAHI_DEPENDENCIES += libdaemon
