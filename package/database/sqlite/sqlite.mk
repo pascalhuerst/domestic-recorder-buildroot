484926bba53d8a526103ac81eeaacf8fe17a40db
Sven Neumann <s.neumann@raumfeld.com>
2011-03-08 22:06:38 +0100

    sqlite: update to version 3.7.5

--- package/database/sqlite/sqlite.mk
+++ package/database/sqlite/sqlite.mk
@@ -4,8 +4,8 @@
 #
 #############################################################
 
-SQLITE_VERSION = 3.7.4
-SQLITE_PACKAGE_VERSION = 3070400
+SQLITE_VERSION = 3.7.5
+SQLITE_PACKAGE_VERSION = 3070500
 SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_PACKAGE_VERSION).tar.gz
 SQLITE_SITE = http://www.sqlite.org
 SQLITE_INSTALL_STAGING = YES
