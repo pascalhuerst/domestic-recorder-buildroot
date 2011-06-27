b445207d0941b59567fcecf64da1a5b2824909b5
Sven Neumann <s.neumann@raumfeld.com>
2011-06-27 18:37:06 +0200

    loopbacktest: update build rules for new buildroot
    This includes a patch for p0-renderer that should eventually be
    committed there.

--- package/raumfeld/loopbacktest/loopbacktest.mk
+++ package/raumfeld/loopbacktest/loopbacktest.mk
@@ -7,7 +7,7 @@ LOOPBACKTEST_VERSION:=$(call qstrip,$(BR2_PACKAGE_RAUMFELD_BRANCH))
 LOOPBACKTEST_DIR:=$(BUILD_DIR)/loopbacktest-$(LOOPBACKTEST_VERSION)
 LOOPBACKTEST_TARGET_DIR:=raumfeld/p0-audiotest
 LOOPBACKTEST_BINARY:=$(LOOPBACKTEST_TARGET_DIR)/p0-audiotest
-LOOPBACKTEST_CROSS_PREFIX:=$(BUILD_DIR)/..
+LOOPBACKTEST_CROSS_PREFIX:=$(BASE_DIR)/..
 
 LOOPBACKTEST_DEPENDENCIES = host-pkg-config alsa-lib liboil libglib2
 
@@ -21,11 +21,12 @@ $(LOOPBACKTEST_DIR)/.bzr:
 	if ! test -d $(LOOPBACKTEST_DIR)/.bzr; then \
 	  	(cd $(BUILD_DIR); \
 		mkdir -p loopbacktest-$(LOOPBACKTEST_VERSION); \
-	 	$(call qstrip,$(BR2_BZR_CO)) $(BR2_PACKAGE_RAUMFELD_REPOSITORY)/p0-renderer/$(LOOPBACKTEST_VERSION) loopbacktest-$(LOOPBACKTEST_VERSION)) \
+	 	$(call qstrip,$(BR2_BZR_CO)) $(BR2_PACKAGE_RAUMFELD_REPOSITORY)/p0-renderer/$(LOOPBACKTEST_VERSION) loopbacktest-$(LOOPBACKTEST_VERSION); \
+		cd loopbacktest-trunk && patch -p0 < ../../../package/raumfeld/p0-renderer/p0-renderer-buildroot.patch) \
 	fi
 	touch -c $@
 
-loopbacktest-source: $(LOOPBACKTEST_DIR)/.bzr 
+loopbacktest-source: $(LOOPBACKTEST_DIR)/.bzr
 
 $(STAGING_DIR)/$(LOOPBACKTEST_BINARY): loopbacktest-source
 	$(MAKE) -C $(LOOPBACKTEST_DIR)/test CROSS=$(LOOPBACKTEST_CROSS) DEST=$(STAGING_DIR)/raumfeld CROSS_PREFIX=$(LOOPBACKTEST_CROSS_PREFIX)
