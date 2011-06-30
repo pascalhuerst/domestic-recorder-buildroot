71098ca0b12cafab744ebba432b63f51b96ec530
Sven Neumann <s.neumann@raumfeld.com>
2011-06-30 11:00:41 +0200

    nettle: new package
    Nettle is a crypto library. It is needed (or actually preferred
    over libgcrypt) by newer versions of gnutls.
    
    Signed-off-by: Sven Neumann <s.neumann@raumfeld.com>

---
+++ package/nettle/nettle.mk
@@ -0,0 +1,14 @@
+##############################################################
+#
+# nettle
+#
+#############################################################
+
+NETTLE_VERSION = 2.1
+NETTLE_SOURCE = nettle-$(NETTLE_VERSION).tar.gz
+NETTLE_SITE =ftp://ftp.lysator.liu.se/pub/security/lsh/
+NETTLE_INSTALL_STAGING = YES
+
+NETTLE_DEPENDENCIES = gmp
+
+$(eval $(call AUTOTARGETS,package,nettle))
