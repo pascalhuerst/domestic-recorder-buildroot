f69d4b991f3e7c44a732f308234c8d26f3c74985
Sven Neumann <s.neumann@raumfeld.com>
2011-06-30 10:39:15 +0200

    libunwind: use new GIT download rule
    Buildroot now offers a more convenient way to use a git snapshot.

--- package/libunwind/libunwind.mk
+++ package/libunwind/libunwind.mk
@@ -5,20 +5,12 @@
 #############################################################
 
 LIBUNWIND_VERSION:=99e60be
-LIBUNWIND_SOURCE:=libunwind-$(LIBUNWIND_VERSION).tar.gz
-LIBUNWIND_URL:=http://git.savannah.gnu.org/gitweb/?p=libunwind.git;a=snapshot;h=$(LIBUNWIND_VERSION);sf=tgz
+LIBUNWIND_SITE=git://git.sv.gnu.org/libunwind.git
+LIBUNWIND_SITE_METHOD=git
 LIBUNWIND_AUTORECONF = YES
 LIBUNWIND_LIBTOOL_PATCH = NO
 LIBUNWIND_INSTALL_STAGING = YES
-LIBUNWIND_INSTALL_TARGET = YES
 
 LIBUNWIND_CONF_OPT = --enable-debug-frame
 
 $(eval $(call AUTOTARGETS,package,libunwind))
-
-$(LIBUNWIND_DIR)/.stamp_downloaded:
-	$(Q)test -e $(DL_DIR)/$(LIBUNWIND_SOURCE) || \
-	$(WGET) -P $(DL_DIR) $(BR2_PRIMARY_SITE)/$(LIBUNWIND_SOURCE) || \
-	$(WGET) -O $(DL_DIR)/$(LIBUNWIND_SOURCE) "$(LIBUNWIND_URL)"
-	$(Q)mkdir -p $(LIBUNWIND_DIR)
-	$(Q)touch $@
