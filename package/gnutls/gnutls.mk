16cd7b2ab7d68d74c244d957e820ff2f378ab778
Sven Neumann <s.neumann@raumfeld.com>
2011-06-30 17:12:17 +0200

    gnutls: patch libtool to avoid relinking

--- package/gnutls/gnutls.mk
+++ package/gnutls/gnutls.mk
@@ -7,7 +7,7 @@
 GNUTLS_VERSION = 2.12.7
 GNUTLS_SOURCE = gnutls-$(GNUTLS_VERSION).tar.bz2
 GNUTLS_SITE = ftp://ftp.gnutls.org/pub/gnutls
-GNUTLS_LIBTOOL_PATCH = NO
+GNUTLS_LIBTOOL_PATCH = YES
 GNUTLS_INSTALL_STAGING = YES
 
 GNUTLS_DEPENDENCIES = host-pkg-config libglib2 nettle
