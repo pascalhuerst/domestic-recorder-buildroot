5ef9e1ced65ccb2b8532884ab4dbc73150067d68
Sven Neumann <s.neumann@raumfeld.com>
2011-06-30 20:57:09 +0200

    taglib: add dependencies on host-pkg-config and libglib2

--- package/multimedia/taglib/taglib.mk
+++ package/multimedia/taglib/taglib.mk
@@ -9,10 +9,11 @@ TAGLIB_SITE = http://developer.kde.org/~wheeler/files/src
 TAGLIB_LIBTOOL_PATCH = NO
 TAGLIB_INSTALL_STAGING = YES
 
+TAGLIB_DEPENDENCIES = host-pkg-config libglib2
+
 TAGLIB_CONF_ENV = \
 	USER_INCLUDES=`PKG_CONFIG_SYSROOT_DIR=$(STAGING_DIR) $(HOST_DIR)/usr/bin/pkg-config --cflags gio-2.0` \
-	USER_LDFLAGS=`PKG_CONFIG_SYSROOT_DIR=$(STAGING_DIR) $(HOST_DIR)/usr/bin/p\
-kg-config --libs gio-2.0` \
+	USER_LDFLAGS=`PKG_CONFIG_SYSROOT_DIR=$(STAGING_DIR) $(HOST_DIR)/usr/bin/pkg-config --libs gio-2.0` \
 	DO_NOT_COMPILE='bindings tests examples' \
 	ac_cv_header_cppunit_extensions_HelperMacros_h=no \
 	ac_cv_header_zlib_h=$(if $(BR2_PACKAGE_ZLIB),yes,no)
