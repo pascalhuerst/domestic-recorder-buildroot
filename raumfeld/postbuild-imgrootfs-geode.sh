007ca9326de8906fe285cf76c7250e4d48b6c77d
Sven Neumann <s.neumann@raumfeld.com>
2011-06-30 20:35:32 +0200

    raumfeld: fix path to gcc in geode postbuild script

--- raumfeld/postbuild-imgrootfs-geode.sh
+++ raumfeld/postbuild-imgrootfs-geode.sh
@@ -9,7 +9,7 @@ cp -r raumfeld/rootfs/etc/raumfeld-version $1/etc
 
 echo "Building and installing test binaries..."
 
-GCC=build_i586/staging_dir/usr/bin/i586-linux-uclibc-gcc
+GCC=output/host/usr/bin/i586-linux-gcc
 
 $GCC -o $1/progress_fb -Wall raumfeld/testsuite/progress_fb/progress_fb.c
 $GCC -o $1/input_test -Wall raumfeld/testsuite/input_test/input_test.c
