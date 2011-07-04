fc148b2b52aa5df5c32314dc5616813f9f204311
Sven Neumann <s.neumann@raumfeld.com>
2011-07-04 15:06:52 +0200

    gnutls: go back to version 2.10.3 and use gcrypt instead of nettle
    Version 2.10.3 is the last that uses gcrypt. We need to use gcrypt
    because users of this library (libsoup and glib-networking) still
    assume that gcrypt is being used and also include gcrypt.h directly.

--- package/gnutls/gnutls.mk
+++ package/gnutls/gnutls.mk
@@ -4,14 +4,16 @@
 #
 #############################################################
 
-GNUTLS_VERSION = 2.12.7
+GNUTLS_VERSION = 2.10.3
 GNUTLS_SOURCE = gnutls-$(GNUTLS_VERSION).tar.bz2
 GNUTLS_SITE = ftp://ftp.gnutls.org/pub/gnutls
 GNUTLS_LIBTOOL_PATCH = YES
 GNUTLS_INSTALL_STAGING = YES
+GNUTLS_INSTALL_TARGET = YES
 
-GNUTLS_DEPENDENCIES = host-pkg-config libglib2 nettle
+GNUTLS_DEPENDENCIES = host-pkg-config libglib2 libgcrypt
 
-GNUTLS_CONF_ENV = LIBS=-ldl
+GNUTLS_CONF_OPT = \
+	--with-libgcrypt-prefix=$(STAGING_DIR)/usr
 
 $(eval $(call AUTOTARGETS,package,gnutls))
