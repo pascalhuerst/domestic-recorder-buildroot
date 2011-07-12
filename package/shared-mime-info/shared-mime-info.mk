4bc500a404bd9874e344b2ace1604114f09b6224
Sven Neumann <s.neumann@raumfeld.com>
2011-07-12 23:05:14 +0200

    shared-mime-info: update to version 0.90
    Need to pass -j1 to make as parallel builds appear to be broken.
    
    Signed-off-by: Sven Neumann <s.neumann@raumfeld.com>

--- package/shared-mime-info/shared-mime-info.mk
+++ package/shared-mime-info/shared-mime-info.mk
@@ -3,7 +3,7 @@
 # shared-mime-info
 #
 #############################################################
-SHARED_MIME_INFO_VERSION = 0.60
+SHARED_MIME_INFO_VERSION = 0.90
 SHARED_MIME_INFO_SOURCE = shared-mime-info-$(SHARED_MIME_INFO_VERSION).tar.bz2
 SHARED_MIME_INFO_SITE = http://freedesktop.org/~hadess
 
@@ -16,10 +16,11 @@ SHARED_MIME_INFO_DEPENDENCIES = host-pkg-config host-libglib2 host-libxml2 host-
 
 SHARED_MIME_INFO_CONF_OPT = --disable-update-mimedb
 
+SHARED_MIME_INFO_MAKE_OPT = -j1
+
 HOST_SHARED_MIME_INFO_DEPENDENCIES = host-pkg-config host-intltool
 
-HOST_SHARED_MIME_INFO_CONF_OPT = \
-	--disable-update-mimedb
+HOST_SHARED_MIME_INFO_CONF_OPT = --disable-update-mimedb
 
 define SHARED_MIME_INFO_INSTALL_TARGET_CMDS
 	$(HOST_MAKE_ENV) $(SHARED_MIME_INFO_HOST_BINARY) $(STAGING_DIR)/usr/share/mime
