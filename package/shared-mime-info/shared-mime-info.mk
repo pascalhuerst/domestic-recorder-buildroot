2d21c3c6dffdacc6253a6ee48de520a73594a48b
Sven Neumann <s.neumann@raumfeld.com>
2011-07-13 14:22:53 +0200

    shared-mime-info: also disable parallel make for the host version

--- package/shared-mime-info/shared-mime-info.mk
+++ package/shared-mime-info/shared-mime-info.mk
@@ -15,12 +15,12 @@ SHARED_MIME_INFO_CONF_ENV = XMLLINT=$(HOST_DIR)/usr/bin/xmllint
 SHARED_MIME_INFO_DEPENDENCIES = host-pkg-config host-libglib2 host-libxml2 host-shared-mime-info libxml2 libglib2
 
 SHARED_MIME_INFO_CONF_OPT = --disable-update-mimedb
-
 SHARED_MIME_INFO_MAKE_OPT = -j1
 
 HOST_SHARED_MIME_INFO_DEPENDENCIES = host-pkg-config host-intltool
 
 HOST_SHARED_MIME_INFO_CONF_OPT = --disable-update-mimedb
+HOST_SHARED_MIME_INFO_MAKE_OPT = -j1
 
 define SHARED_MIME_INFO_INSTALL_TARGET_CMDS
 	$(HOST_MAKE_ENV) $(SHARED_MIME_INFO_HOST_BINARY) $(STAGING_DIR)/usr/share/mime
