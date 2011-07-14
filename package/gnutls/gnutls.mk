bd48a0eefe9998fe4f815504000b3a5f32989b19
Sven Neumann <s.neumann@raumfeld.com>
2011-07-14 09:04:10 +0200

    gnutls: go back to upstream version

--- package/gnutls/gnutls.mk
+++ package/gnutls/gnutls.mk
@@ -4,16 +4,11 @@
 #
 #############################################################
 
-GNUTLS_VERSION = 2.10.3
+GNUTLS_VERSION = 2.8.6
 GNUTLS_SOURCE = gnutls-$(GNUTLS_VERSION).tar.bz2
-GNUTLS_SITE = ftp://ftp.gnutls.org/pub/gnutls
-GNUTLS_LIBTOOL_PATCH = YES
+GNUTLS_SITE = http://ftp.gnu.org/gnu/gnutls/
+GNUTLS_DEPENDENCIES = libgcrypt
+GNUTLS_CONF_OPT += --without-libgcrypt-prefix
 GNUTLS_INSTALL_STAGING = YES
-GNUTLS_INSTALL_TARGET = YES
-
-GNUTLS_DEPENDENCIES = host-pkg-config libglib2 libgcrypt
-
-GNUTLS_CONF_OPT = \
-	--with-libgcrypt-prefix=$(STAGING_DIR)/usr
 
 $(eval $(call AUTOTARGETS,package,gnutls))
