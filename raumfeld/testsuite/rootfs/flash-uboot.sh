9dc016f1ab0431e13375b181c1f7b2fbc31f4c2d
Sven Neumann <s.neumann@raumfeld.com>
2011-07-21 21:44:36 +0200

    raumfeld: update flash-uboot.sh for the new boot-loaders
    Use the new update-boardrev tool to create a boot-loader image with
    the proper hardware revision.

--- raumfeld/testsuite/rootfs/flash-uboot.sh
+++ raumfeld/testsuite/rootfs/flash-uboot.sh
@@ -1,41 +1,45 @@
 #!/bin/sh
 
 hw=$(cat /proc/cpuinfo | grep ^Hardware | cut -f 3 -d' ')
-rev=$(cat /proc/cpuinfo | grep ^Revision | cut -f 2 -d: | cut -f2 -d' ')
+rev=$(fw_printenv boardrev | cut -f 2 -d '=')
 
 # ancient version of our hardware do not report the revision.
 # default to '1' in this case.
 
-if [ "$rev" == "0000" ]; then
-	rev="0001"
+if [ -z "$rev" ]; then
+	rev="0x1"
 fi
 
 case "$hw" in
 	Controller)
-		img="raumfeld-controller-$rev.bin"
+		img="/raumfeld-controller.bin"
 		;;
 	Connector)
-		img="raumfeld-connector-$rev.bin"
+		img="/raumfeld-connector.bin"
 		;;
 	Speaker)
-		img="raumfeld-speaker-$rev.bin"
+		img="/raumfeld-speaker.bin"
 		;;
 	*)
-		echo "Failed to parse machine data. Ups."
+		echo "Failed to parse machine data. Oops."
 		exit 0
 esac
 
-if [ ! -f /$img ]; then
-	echo "/$img does not exist. Bummer."
+if [ ! -f $img ]; then
+	echo "$img does not exist. Bummer."
 	exit 0
 fi
 
+# update the revision
+cp /$img /tmp && img=/tmp/$img
+./update-boardrev $img $rev
+
 # write the bootloader
-dd bs=1024 count=640 if=/$img of=/dev/mtdblock0
+dd bs=1024 count=640 if=$img of=/dev/mtdblock0
 
 # reset the environment, save ethaddr if already set
 eval $(fw_printenv ethaddr)
-dd bs=1024 count=128 skip=640 if=/$img of=/dev/mtdblock1
+dd bs=1024 count=128 skip=640 if=$img of=/dev/mtdblock1
 if [ "$ethaddr" ]; then
 	fw_setenv ethaddr $ethaddr
 	# force the bootloader to do a env reset with its defaults
@@ -43,5 +47,4 @@ if [ "$ethaddr" ]; then
 fi
 
 # write the bootloader splash image
-dd bs=1024 count=384 skip=768 if=/$img of=/dev/mtdblock2
-
+dd bs=1024 count=384 skip=768 if=$img of=/dev/mtdblock2
