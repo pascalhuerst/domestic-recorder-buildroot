4e7ed6ff41f96ccac75dc6ee5976fdfe6c9b5a62
Sven Neumann <s.neumann@raumfeld.com>
2011-08-19 10:17:16 +0200

    libunwind: add patch to check for backtrace support
    Some of the tests won't compile without backtrace. This patch
    from upstream adds a check to disable them if backtrace support
    is missing.

--- package/libunwind/libunwind.mk
+++ package/libunwind/libunwind.mk
@@ -6,6 +6,7 @@
 
 LIBUNWIND_VERSION = 1.0-rc1
 LIBUNWIND_SITE = http://download.savannah.gnu.org/releases/libunwind/
+LIBUNWIND_AUTORECONF = YES
 LIBUNWIND_INSTALL_STAGING = YES
 
 LIBUNWIND_CONF_OPT = --enable-debug-frame
