d163f3fb51236574aca34a57e3d81976fbd7b683
Sven Neumann <s.neumann@raumfeld.com>
2011-08-19 15:03:05 +0200

    libunwind: go back to git snapshot 99e60be
    With version 1.0-rc1 we can't unwind over a signal handler any longer.
    Revert the update until a solution has been found.

--- package/libunwind/libunwind.mk
+++ package/libunwind/libunwind.mk
@@ -4,8 +4,9 @@
 #
 #############################################################
 
-LIBUNWIND_VERSION = 1.0-rc1
-LIBUNWIND_SITE = http://download.savannah.gnu.org/releases/libunwind/
+LIBUNWIND_VERSION:=99e60be
+LIBUNWIND_SITE=git://git.sv.gnu.org/libunwind.git
+LIBUNWIND_SITE_METHOD=git
 LIBUNWIND_AUTORECONF = YES
 LIBUNWIND_INSTALL_STAGING = YES
 
