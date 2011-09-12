fd10338e417488dfbb4d7bb94e3aca8604d6dd1f
Sven Neumann <s.neumann@raumfeld.com>
2011-09-12 11:35:24 +0200

    libunwind: bump to 1.0.1

--- package/libunwind/libunwind.mk
+++ package/libunwind/libunwind.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBUNWIND_VERSION = 1.0-rc1
+LIBUNWIND_VERSION = 1.0.1
 LIBUNWIND_SITE = http://download.savannah.gnu.org/releases/libunwind/
 LIBUNWIND_AUTORECONF = YES
 LIBUNWIND_INSTALL_STAGING = YES
