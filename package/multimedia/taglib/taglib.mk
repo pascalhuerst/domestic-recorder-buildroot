a6847ce3fed7e476ce8adcda6661df03abb86452
Sven Neumann <s.neumann@raumfeld.com>
2011-09-23 10:39:04 +0200

    taglib: make MP4 and WMA support optional
    Signed-off-by: Sven Neumann <s.neumann@raumfeld.com>

--- package/multimedia/taglib/taglib.mk
+++ package/multimedia/taglib/taglib.mk
@@ -7,8 +7,15 @@ TAGLIB_VERSION = 1.7
 TAGLIB_SOURCE = taglib-$(TAGLIB_VERSION).tar.gz
 TAGLIB_SITE = http://developer.kde.org/~wheeler/files/src
 TAGLIB_INSTALL_STAGING = YES
-TAGLIB_CONF_OPT = -DWITH_ASF=1 -DWITH_MP4=1
 TAGLIB_DEPENDENCIES = host-pkg-config libglib2
+
+ifeq ($(BR2_PACKAGE_TAGLIB_ASF),y)
+TAGLIB_CONF_OPT += -DWITH_ASF=ON
+endif
+
+ifeq ($(BR2_PACKAGE_TAGLIB_MP4),y)
+TAGLIB_CONF_OPT += -DWITH_MP4=ON
+endif
 
 define TAGLIB_INSTALL_TARGET_CMDS
 	cp -a $(STAGING_DIR)/usr/lib/libtag.so* $(TARGET_DIR)/usr/lib
