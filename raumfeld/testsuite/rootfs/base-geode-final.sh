b64cfd634fa0672dc7c546e1eb3040d574126974
Sven Neumann <s.neumann@raumfeld.com>
2011-10-10 10:06:07 +0200

    raumfeld: base-geode-final.sh should not be interactive

--- raumfeld/testsuite/rootfs/base-geode-final.sh
+++ raumfeld/testsuite/rootfs/base-geode-final.sh
@@ -15,7 +15,7 @@ TMPROOT=/tmp/root
 ./leds-blink 1 &
 pid=$!
 
-dialog_msg "Mounting filesystems ..."
+echo "Mounting filesystems ..."
 
 mkdir $TMPROOT
 mount -t ext3 -o rw,sync ${DEV}2 $TMPROOT
@@ -24,13 +24,17 @@ mount -t ext3 -o rw,sync ${DEV}1 $TMPROOT/boot
 zcat /rootfs.tgz | tar -f - -C $TMPROOT -xv | \
 	/percent `cat /rootfs.tgz.numfiles` | \
 	dialog_progress "Copying files to harddisk. Please wait." $DIALOGOPTS
+sync
 
-dialog_msg "Unmounting filesystems ..."
+echo "Unmounting filesystems ..."
 
 chmod 0755 $TMPROOT
 umount $TMPROOT/boot
 umount $TMPROOT
 
 kill $pid
-
 ./leds-blink 3
+
+sleep 5
+
+reboot
