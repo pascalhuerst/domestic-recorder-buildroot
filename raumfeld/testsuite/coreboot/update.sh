46bf5d58be4884fd93433fbf9d0e1ef0cf80b191
Sven Neumann <s.neumann@raumfeld.com>
2012-08-22 14:59:42 +0200

    raumfeld: fix flashrom command-line parameters for base-coreboot

--- raumfeld/testsuite/coreboot/update.sh
+++ raumfeld/testsuite/coreboot/update.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/sbin/flashrom -p internal:laptop=this_is_not_a_laptop -w /raumfeld-base.rom
+/usr/sbin/flashrom -p internal:laptop=this_is_not_a_laptop,boardmismatch=force -w /raumfeld-base.rom
