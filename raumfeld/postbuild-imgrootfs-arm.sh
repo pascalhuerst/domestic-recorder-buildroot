pascalhuerst/domestic-recorder-buildroot

commit 1c34e05060e7597eefcbd8295b046dc416df6ccf

raumfeld: also use postbuild-cleanup.sh for imgrootfs

raumfeld/postbuild-imgrootfs-arm.sh
@@ -12,10 +12,11 @@ cp -av raumfeld/rootfs-arm/lib $1
 
 echo "Building and installing test binaries..."
 
-GCC=output/host/usr/bin/arm-linux-gcc
+GCC=$2-gcc
 
 $GCC -o $1/progress_fb -Wall raumfeld/testsuite/progress_fb/progress_fb.c
 $GCC -o $1/input_test -Wall raumfeld/testsuite/input_test/input_test.c
 $GCC -o $1/percent -Wall raumfeld/testsuite/percent/percent.c
 $GCC -o $1/wireless_scan -Wall raumfeld/testsuite/wireless_scan/wireless_scan.c
 
+raumfeld/postbuild-cleanup.sh $*

raumfeld/postbuild-imgrootfs-geode.sh
@@ -9,10 +9,11 @@ cp -r raumfeld/rootfs/etc/raumfeld-version $1/etc
 
 echo "Building and installing test binaries..."
 
-GCC=output/host/usr/bin/i586-linux-gcc
+GCC=$2-gcc
 
 $GCC -o $1/progress_fb -Wall raumfeld/testsuite/progress_fb/progress_fb.c
 $GCC -o $1/input_test -Wall raumfeld/testsuite/input_test/input_test.c
 $GCC -o $1/percent -Wall raumfeld/testsuite/percent/percent.c
 $GCC -o $1/wireless_scan -Wall raumfeld/testsuite/wireless_scan/wireless_scan.c
 
+raumfeld/postbuild-cleanup.sh $*

commit ba29052a569545c0fe1b2a505d5999c02c0f2cd6

raumfeld: fix post-build scripts for imgrootfs targets
This broke with commit 1c34e05060 which was made under the
wrong assumption that the script would be called with the
host dir as second parameter

raumfeld/postbuild-imgrootfs-arm.sh
@@ -2,6 +2,8 @@
 #
 # post-build.sh for the imgrootfs-arm target
 
+echo "postbuild-imgrootfs-arm.sh $1 $2"
+
 echo "Populating the root filesystem ..."
 
 # raumfeld version
@@ -9,7 +11,7 @@ cp -r raumfeld/rootfs/etc/raumfeld-version $1/etc
 
 echo "Building and installing test binaries..."
 
-GCC=$2gcc
+GCC=output/host/usr/bin/arm-linux-gcc
 
 $GCC -o $1/progress_fb -Wall raumfeld/testsuite/progress_fb/progress_fb.c
 $GCC -o $1/input_test -Wall raumfeld/testsuite/input_test/input_test.c

raumfeld/postbuild-imgrootfs-geode.sh
@@ -9,7 +9,7 @@ cp -r raumfeld/rootfs/etc/raumfeld-version $1/etc
 
 echo "Building and installing test binaries..."
 
-GCC=$2gcc
+GCC=output/host/usr/bin/i586-linux-gcc
 
 $GCC -o $1/progress_fb -Wall raumfeld/testsuite/progress_fb/progress_fb.c
 $GCC -o $1/input_test -Wall raumfeld/testsuite/input_test/input_test.c