6dc967fdedeecca0b67a9973c839611d3b727bdd
Sven Neumann <s.neumann@raumfeld.com>
2012-10-09 12:22:57 +0200

    libunwind: bump to version 1.1
    Signed-off-by: Sven Neumann <s.neumann@raumfeld.com>

--- package/libunwind/libunwind.mk
+++ package/libunwind/libunwind.mk
@@ -4,9 +4,8 @@
 #
 #############################################################
 
-LIBUNWIND_VERSION = 1.0.1
+LIBUNWIND_VERSION = 1.1
 LIBUNWIND_SITE = http://download.savannah.gnu.org/releases/libunwind/
-LIBUNWIND_AUTORECONF = YES
 LIBUNWIND_INSTALL_STAGING = YES
 
 $(eval $(autotools-package))
