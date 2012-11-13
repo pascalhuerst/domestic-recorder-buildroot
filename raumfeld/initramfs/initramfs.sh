pascalhuerst/domestic-recorder-buildroot

commit 0e5dae8918a74362b26f2c1bcfc48107c281adfc

raumfeld: minor cleanup, mostly whitespace changes

raumfeld/initramfs/initramfs.sh
@@ -5,132 +5,133 @@ export PATH="/sbin:/usr/sbin:$PATH"
 echo "Booted to initramfs."
 
 hw=`cat /proc/cpuinfo | grep ^Hardware | cut -f 3 -d' '`
-offset="5128192"
 
 if [ -z "$hw" ]; then
-	hw=`cat /proc/cpuinfo | grep ^model\ name | cut -f 3 -d' '`
+    hw=`cat /proc/cpuinfo | grep ^model\ name | cut -f 3 -d' '`
 fi
 
 case "$hw" in
-  AM33XX)
-		arch="armada"
-		img="connect2.img"
+    AM33XX)
+	arch="armada"
+	img="connect2.img"
         offset="6561792"
         bootloader="raumfeld-am3xx.bin"
-    ;;
-	Controller)
-		arch="arm"
-		img="control.img"
-                bootloader="raumfeld-controller.bin"
-		;;
-	Connector)
-		arch="arm"
-		img="connect.img"
-                bootloader="raumfeld-connector.bin"
-		;;
-	Speaker)
-		arch="arm"
-		img="speaker.img"
-                bootloader="raumfeld-speaker.bin"
-		;;
-	Geode*)
-		arch="geode"
-		img="base.img"
-                bios="raumfeld-base.rom"
-		;;
-	*)
-		img="uImage"
-		echo "unknown hardware type '$hw'"
-		;;
+        ;;
+    Controller)
+	arch="arm"
+	img="control.img"
+        offset="5128192"
+        bootloader="raumfeld-controller.bin"
+	;;
+    Connector)
+	arch="arm"
+	img="connect.img"
+        offset="5128192"
+        bootloader="raumfeld-connector.bin"
+	;;
+    Speaker)
+	arch="arm"
+	img="speaker.img"
+        offset="5128192"
+        bootloader="raumfeld-speaker.bin"
+	;;
+    Geode*)
+	arch="geode"
+	img="base.img"
+        bios="raumfeld-base.rom"
+	;;
+    *)
+	img="uImage"
+	echo "unknown hardware type '$hw'"
+	;;
 esac
 
 if [ "$(grep raumfeld-update /proc/cmdline)" ]; then
-	param=$(cat /proc/cmdline | sed -e 's/^.*raumfeld-update=//' -e 's/ .*$//')
-	img=$(echo $param | cut -d, -f1)
-	numfiles=$(echo $param | cut -d, -f2)
+    param=$(cat /proc/cmdline | sed -e 's/^.*raumfeld-update=//' -e 's/ .*$//')
+    img=$(echo $param | cut -d, -f1)
+    numfiles=$(echo $param | cut -d, -f2)
 
-	echo "Image name $img ($numfiles files)"
-	echo "Performing software update ..."
+    echo "Image name $img ($numfiles files)"
+    echo "Performing software update ..."
 
-	mkdir -p /mnt
-	mkdir -p /update
+    mkdir -p /mnt
+    mkdir -p /update
 
-	case "$arch" in
-		arm)
-			mount -t ubifs -o rw ubi:RootFS /mnt
-			mount -t ubifs -o ro ubi0:Updates /update
-			update=/update/$img
-			;;
-		geode)
-			mount -t ext3 -o rw,data=writeback /dev/hda2 /mnt
-			mount -t ext3 -o rw,data=writeback /dev/hda1 /mnt/boot
-			update=/mnt/update/$img
-			;;
-		*)
-			echo "unknown architecture '$arch'"
-			;;
-	esac
+    case "$arch" in
+	arm)
+	    mount -t ubifs -o rw ubi:RootFS /mnt
+	    mount -t ubifs -o ro ubi0:Updates /update
+	    update=/update/$img
+	    ;;
+	geode)
+	    mount -t ext3 -o rw,data=writeback /dev/hda2 /mnt
+	    mount -t ext3 -o rw,data=writeback /dev/hda1 /mnt/boot
+	    update=/mnt/update/$img
+	    ;;
+	*)
+	    echo "unknown architecture '$arch'"
+	    ;;
+    esac
 
-	if [ -n "$bootloader" ]; then
-                gunzip -c $update | tar x ./tmp/$bootloader
-	        echo "Checking the boot-loader ..."
-                (cd /tmp; /update-uboot.sh; rm -f $bootloader)
-        fi
+    if [ -n "$bootloader" ]; then
+        gunzip -c $update | tar x ./tmp/$bootloader
+	echo "Checking the boot-loader ..."
+        (cd /tmp; /update-uboot.sh; rm -f $bootloader)
+    fi
 
-	if [ -n "$bios" ]; then
-                gunzip -c $update | tar x ./tmp/$bios
-	        echo "Checking the BIOS ..."
-                (cd /tmp; /update-coreboot.sh; rm -f $bios)
-        fi
+    if [ -n "$bios" ]; then
+        gunzip -c $update | tar x ./tmp/$bios
+	echo "Checking the BIOS ..."
+        (cd /tmp; /update-coreboot.sh; rm -f $bios)
+    fi
 
-	cd /mnt
-	raumfeld-extract-update $update $numfiles
-	cd /
-        sync
+    cd /mnt
+    raumfeld-extract-update $update $numfiles
+    cd /
+    sync
 
-	case "$arch" in
-		arm)
-                        umount /update
-			umount /mnt
-			;;
-		geode)
-                        sleep 5
-                        umount /mnt/boot
-                        umount /mnt
-                        sleep 5
-			;;
-		*)
-			echo "unknown architecture '$arch'"
-			;;
-	esac
+    case "$arch" in
+	arm)
+            umount /update
+	    umount /mnt
+	    ;;
+	geode)
+            sleep 5
+            umount /mnt/boot
+            umount /mnt
+            sleep 5
+	    ;;
+	*)
+	    echo "unknown architecture '$arch'"
+	    ;;
+    esac
 
-	echo "Rebooting ..."
-        reboot
+    echo "Rebooting ..."
+    reboot
 
 else
-	echo "Image name $img"
-	echo "Waiting for USB device to appear ..."
+    echo "Image name $img"
+    echo "Waiting for USB device to appear ..."
 
-	while [ -z "$(grep sda /proc/partitions)" ]; do
-		sleep 1
-	done
+    while [ -z "$(grep sda /proc/partitions)" ]; do
+	sleep 1
+    done
 
-        # It takes a while for partitions to be recognized after the disk
-        # was found.  Sleep three more seconds...
-        sleep 3
+    # It takes a while for partitions to be recognized after the disk
+    # was found.  Sleep three more seconds...
+    sleep 3
 
-	mkdir /usb
+    mkdir /usb
 
-	mount /dev/sda1 /usb || mount /dev/sda /usb
-	losetup -o $offset /dev/loop0 /usb/$img
+    mount /dev/sda1 /usb || mount /dev/sda /usb
+    losetup -o $offset /dev/loop0 /usb/$img
 
-	mkdir /rootfs
-	mount -t ext2 -o ro /dev/loop0 /rootfs
+    mkdir /rootfs
+    mount -t ext2 -o ro /dev/loop0 /rootfs
 
-	echo "Jumping to the newly mounted rootfs"
-	chroot /rootfs /init.sh
+    echo "Jumping to the newly mounted rootfs"
+    chroot /rootfs /init.sh
 fi
 
-# shouldn't get here
+# shouldn't ever get here
 exec /bin/sh
-

commit b2df955c612a15aa9b73949bd530c2a9d52734c5

raumfeld: set offset for all platforms
This broke with 0e5dae8. Fixes bug #2962.

raumfeld/initramfs/initramfs.sh
@@ -10,6 +10,8 @@ if [ -z "$hw" ]; then
     hw=`cat /proc/cpuinfo | grep ^model\ name | cut -f 3 -d' '`
 fi
 
+offset="5128192"  # default value
+
 case "$hw" in
     AM33XX)
 	arch="armada"
@@ -20,19 +22,16 @@ case "$hw" in
     Controller)
 	arch="arm"
 	img="control.img"
-        offset="5128192"
         bootloader="raumfeld-controller.bin"
 	;;
     Connector)
 	arch="arm"
 	img="connect.img"
-        offset="5128192"
         bootloader="raumfeld-connector.bin"
 	;;
     Speaker)
 	arch="arm"
 	img="speaker.img"
-        offset="5128192"
         bootloader="raumfeld-speaker.bin"
 	;;
     Geode*)