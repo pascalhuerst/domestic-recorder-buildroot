7f9b1d491f6c14dbd70d397280f923ac1b6ddc80
Daniel Mack <zonque@gmail.com>
2012-11-13 11:00:43 +0800

    raumfeld: imginfo: switch to getopt

--- raumfeld/imgtool/imginfo.c
+++ raumfeld/imgtool/imginfo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -9,22 +10,46 @@
 
 static void usage(const char *argv0)
 {
-	printf("Usage: %s <imgfile> [version]\n", argv0);
-	exit(-1);
+	printf("Usage: %s [options] <imgfile> [version]\n", argv0);
+	printf("        -v, --version <number>       Layout version (optional, defaults to 0)\n");
+	printf("        -h, --help                   This help output\n");
 }
+
+static struct option long_options[] = {
+	{ "version",		required_argument,	0,	'v' },
+	{ "help",		no_argument,		0,	'h' },
+	{ NULL, 0, 0, 0 }
+};
 
 int main(int argc, char **argv)
 {
 	int fd, ret = 0, version = 0;
 	char *imgname;
 
-	if (argc < 2)
+	while (1) {
+		int option_index = 0;
+		int c = getopt_long (argc, argv, "v:h",
+				     long_options, &option_index);
+		if (c < 0)
+			break;
+
+		switch (c) {
+		case 'v':
+			version = strtol(optarg, 0, 10);
+			break;
+		case 'h':
+		default:
+			usage(argv[0]);
+			return -1;
+		}
+	}
+
+	if (argc <= optind) {
 		usage(argv[0]);
+		return -1;
+	}
 
-	if (argc > 2)
-		version = strtol(argv[2], NULL, 10);
-
-	imgname = argv[1];
+	imgname = argv[optind];
 	fd = open(imgname, O_RDONLY);
 	if (fd < 0)
 		perror("open");
