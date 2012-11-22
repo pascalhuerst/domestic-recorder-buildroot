0b4258d97196806c3edc56c5e673b57ca98eba0a
Sven Neumann <s.neumann@raumfeld.com>
2012-11-22 17:55:45 +0100

    raumfeld: set ARM_TYPE for the stream-decoder

--- package/raumfeld/stream-decoder/stream-decoder.mk
+++ package/raumfeld/stream-decoder/stream-decoder.mk
@@ -6,6 +6,10 @@
 
 STREAM_DECODER_INSTALL_STAGING = YES
 
+ifeq ($(ARCH),arm)
+STREAM_DECODER_CONF_ENV = ARM_TYPE="$(call qstrip,$(BR2_ARM_TYPE))"
+endif
+
 STREAM_DECODER_CONF_OPT = --disable-glibtest
 
 STREAM_DECODER_DEPENDENCIES = host-pkg-config libsoup gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-ffmpeg
