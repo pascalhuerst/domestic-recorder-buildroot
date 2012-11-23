a6be4eca60b6c82814e0ab63cfc25d9930fbaa7b
Sven Neumann <s.neumann@raumfeld.com>
2012-11-23 15:20:37 +0100

    raumfeld: clean up device-tree files
    Turn am33xx-raumfeld-connector.dts into a .dtsi file so that it
    can get included. Rename files to include module and baseboard
    versions. Add am33xx-raumfeld-connector-0-0.dts that just includes
    the proper files.

--- raumfeld/dts/make.sh
+++ raumfeld/dts/make.sh
@@ -2,22 +2,17 @@
 
 set -e
 
-TARGETS=am33xx-raumfeld-connector
-
-DIR=$(mktemp -d)
-
 # take buildroot's dtc and mkcramfs
 HOST_DIR=../../output/host/
 MKFS=${HOST_DIR}/usr/bin/mkcramfs
 DTC=${HOST_DIR}/usr/bin/dtc
 
+TARGETS=am33xx-raumfeld-connector-0.0.dtb
 
-# this is hack
-module_version=0
-baseboard_version=0
+DIR=$(mktemp -d)
 
 for TARGET in ${TARGETS}; do
-	${DTC} -o ${DIR}/${TARGET}-${module_version}-${baseboard_version}.dtb -O dtb ${TARGET}.dts
+	${DTC} -o ${DIR}/${TARGET}.dtb -O dtb ${TARGET}.dts
 done
 
 ${MKFS} ${DIR} dts.cramfs
