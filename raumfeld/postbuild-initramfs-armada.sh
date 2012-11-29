36d0a0096b21544d1b14976f602d0301e6c7cec0
Sven Neumann <s.neumann@raumfeld.com>
2012-11-29 21:23:28 +0100

    raumfeld: add extra postbuild script for initramsfs-armada
    We don't need the tools for replacing the boot-loader on the Armada
    platform (at least not yet).

---
+++ raumfeld/postbuild-initramfs-armada.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# post-build.sh for the initramfs-armada target
+
+cp raumfeld/initramfs/initramfs.sh $1
+
+cat << __END__ > $1/etc/inittab
+null::sysinit:/bin/mount -o remount,rw /
+null::sysinit:/bin/mount -t proc proc /proc
+null::sysinit:/bin/mount -a
+console::sysinit:/initramfs.sh
+__END__
