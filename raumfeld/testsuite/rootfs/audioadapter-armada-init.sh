ec14393ade625b5de5b15f50ae62c442a6514a07
Sven Neumann <s.neumann@raumfeld.com>
2013-02-08 15:59:05 +0100

    raumfeld: remove obsolete code from armada-init script

--- raumfeld/testsuite/rootfs/audioadapter-armada-init.sh
+++ raumfeld/testsuite/rootfs/audioadapter-armada-init.sh
@@ -1,30 +1,3 @@
 #!/bin/sh
 
-exit -1
-
-source tests.inc
-
-cd tests
-
-if [ ! -z "$(grep -i speaker /proc/cpuinfo)" ]; then
-	./nand     		&& \
-	./wifi			&& \
-	./ethaddr  		&& \
-	./ethernet 		&& \
-	./rotary		&& \
-	./zerosetup-button
-else
-	./nand     		&& \
-	./wifi     		&& \
-	./ethaddr  		&& \
-	./ethernet 		&& \
-	./audio			&& \
-	./zerosetup-button
-fi
-
-./audio-loopback >/dev/null 2>&1 &
-
-test_result
-
-./test-menu
-
+exit 0
