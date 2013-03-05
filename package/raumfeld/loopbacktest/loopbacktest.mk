715774193cbc3da5e6a287a3591b876d07f810ac
Sven Neumann <s.neumann@raumfeld.com>
2013-03-05 23:00:16 +0100

    raumfeld: remove orc dependency from loopbacktest

--- package/raumfeld/loopbacktest/loopbacktest.mk
+++ package/raumfeld/loopbacktest/loopbacktest.mk
@@ -8,6 +8,6 @@ LOOPBACKTEST_MODULE = p0-renderer
 LOOPBACKTEST_SOURCE_DIR = $(LOOPBACKTEST_DIR)/test
 LOOPBACKTEST_TARGET_DIR = $(TARGET_DIR)/raumfeld/p0-audiotest
 
-LOOPBACKTEST_DEPENDENCIES = host-pkgconf host-orc alsa-lib orc libglib2
+LOOPBACKTEST_DEPENDENCIES = host-pkgconf alsa-lib libglib2
 
 $(eval $(raumfeld-cross-package))
