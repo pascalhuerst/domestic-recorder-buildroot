822546e9aded1721bf702f6eedf7cdab101b26ef
Sven Neumann <s.neumann@raumfeld.com>
2013-04-11 20:07:10 +0200

    remote-control: add hook to run glib-gettextize before autoreconf
    Together with the changes in remote-control this makes the
    package compile again after the buildroot update.

--- package/raumfeld/remote-control/remote-control.mk
+++ package/raumfeld/remote-control/remote-control.mk
@@ -4,6 +4,12 @@
 #
 #############################################################
 
+define REMOTE_CONTROL_GLIB_GETTEXTIZE
+	(cd $(@D); $(HOST_DIR)/usr/bin/glib-gettextize)
+endef
+
+REMOTE_CONTROL_POST_PATCH_HOOKS += REMOTE_CONTROL_GLIB_GETTEXTIZE
+
 REMOTE_CONTROL_CONF_ENV = \
 	ac_cv_path_GLIB_GENMARSHAL=$(HOST_DIR)/usr/bin/glib-genmarshal \
 	ac_cv_path_GLIB_MKENUMS=$(HOST_DIR)/usr/bin/glib-mkenums \
