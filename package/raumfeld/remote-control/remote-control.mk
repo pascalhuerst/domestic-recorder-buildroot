6962e46af782b8ebbbfae6e23f68bb3501f80797
Sven Neumann <s.neumann@raumfeld.com>
2013-05-13 15:34:54 +0200

    remote-control: minor cleanup

--- package/raumfeld/remote-control/remote-control.mk
+++ package/raumfeld/remote-control/remote-control.mk
@@ -12,8 +12,7 @@ REMOTE_CONTROL_POST_PATCH_HOOKS += REMOTE_CONTROL_GLIB_GETTEXTIZE
 
 REMOTE_CONTROL_CONF_ENV = \
 	ac_cv_path_GLIB_GENMARSHAL=$(HOST_DIR)/usr/bin/glib-genmarshal \
-	ac_cv_path_GLIB_MKENUMS=$(HOST_DIR)/usr/bin/glib-mkenums \
-	gt_cv_func_gnugettext1_libintl=yes
+	ac_cv_path_GLIB_MKENUMS=$(HOST_DIR)/usr/bin/glib-mkenums
 
 REMOTE_CONTROL_CONF_OPT = \
 	--disable-glibtest
