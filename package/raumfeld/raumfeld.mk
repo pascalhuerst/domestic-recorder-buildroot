d17b1fc2418c773d630f52c4051c10c7d55772ae
Sven Neumann <neumann@teufel.de>
2013-07-04 10:51:32 +0200

    raumfeld: add a dependency on the raumfeld-repo source tree
    Also add new target "raumfeld-dirclean" that wipes out all the
    Raumfeld modules.

--- package/raumfeld/raumfeld.mk
+++ package/raumfeld/raumfeld.mk
@@ -8,9 +8,7 @@
 
 define inner-raumfeld-cross-package
 
-
 $(2)_OVERRIDE_SRCDIR = $(BUILD_DIR)/raumfeld-repo-$(call qstrip,$(BR2_PACKAGE_RAUMFELD_REPO_VERSION))/$(1)
-
 
 ifeq ($(ARCH),arm)
   CROSS = ARM
@@ -19,7 +17,6 @@ endif
 ifeq ($(ARCH),i586)
   CROSS = GEODE
 endif
-
 
 define $(2)_BUILD_CMDS
   $(MAKE) -C $$($(2)_SRCDIR) CROSS=$$(CROSS) $$(EXTRA_MAKE_OPTS) DEST=$(TARGET_DIR)/raumfeld CROSS_PREFIX=$(BASE_DIR)
@@ -30,14 +27,14 @@ define $(2)_CLEAN_CMDS
   $(MAKE) -C $$($(2)_DIR) CROSS=$$(CROSS) clean
 endef
 
-
 RAUMFELD_DIRCLEAN_TARGETS += $(1)-dirclean
-
 
 # Call the generic package infrastructure to generate the necessary
 # make targets
 $(call inner-generic-package,$(1),$(2),$(2),$(3),target)
 
+# The rsync target depends on raumfeld-repo to be extracted
+$$($(2)_TARGET_RSYNC): raumfeld-repo-extract
 
 endef # inner-raumfeld-cross-package
 
@@ -51,7 +48,6 @@ endef # inner-raumfeld-cross-package
 ################################################################################
 
 define inner-raumfeld-autotools-package
-
 
 $(2)_OVERRIDE_SRCDIR = $(BUILD_DIR)/raumfeld-repo-$(call qstrip,$(BR2_PACKAGE_RAUMFELD_REPO_VERSION))/$(1)
 
@@ -73,13 +69,14 @@ ifeq ($$($(2)_GTKDOCIZE),YES)
 $(2)_PRE_CONFIGURE_HOOKS += $(2)_GTKDOCIZE_HOOK
 endif
 
-
 RAUMFELD_DIRCLEAN_TARGETS += $(1)-dirclean
 
 # Call the generic autotools package infrastructure to generate the necessary
 # make targets
 $(call inner-autotools-package,$(1),$(2),$(2),$(3),target)
 
+# The rsync target depends on raumfeld-repo to be extracted
+$$($(2)_TARGET_RSYNC): raumfeld-repo-extract
 
 endef # inner-raumfeld-autotools-package
 
@@ -103,8 +100,8 @@ raumfeld-cross-package = $(call inner-raumfeld-cross-package,$(call pkgname),$(c
 ################################################################################
 ################################################################################
 
-RAUMFELD_DIRCLEAN_TARGETS =
+RAUMFELD_DIRCLEAN_TARGETS = raumfeld-repo-dirclean
 
 include package/raumfeld/*/*.mk
 
-raumfeld-rebuild: $(RAUMFELD_DIRCLEAN_TARGETS) all
+raumfeld-dirclean: $(RAUMFELD_DIRCLEAN_TARGETS)
