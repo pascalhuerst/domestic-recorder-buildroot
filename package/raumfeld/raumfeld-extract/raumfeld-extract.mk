47a90812a3ca0579a846ee49aa9f24d494624831
Sven Neumann <neumann@teufel.de>
2013-07-05 15:42:32 +0200

    raumfeld: fix build of raumfeld-extract
    raumfeld-extract has a somewhat unfortunate name as there are implicit
    pkg-extract rules in buildroot

--- package/raumfeld/raumfeld-extract/raumfeld-extract.mk
+++ package/raumfeld/raumfeld-extract/raumfeld-extract.mk
@@ -4,6 +4,8 @@
 #
 #############################################################
 
+RAUMFELD_EXTRACT_MODULE = raumfeld-extract
+
 RAUMFELD_EXTRACT_DEPENDENCIES = libarchive
 
 $(eval $(raumfeld-autotools-package))
