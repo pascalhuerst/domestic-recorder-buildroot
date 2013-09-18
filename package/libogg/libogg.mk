ef7b8649a513677a0b40d9c1b1e82101b1e7ab21
Sven Neumann <neumann@teufel.de>
2013-09-18 23:16:49 +0200

    libogg: bump to version 1.3.1
    Signed-off-by: Sven Neumann <neumann@teufel.de>

--- package/libogg/libogg.mk
+++ package/libogg/libogg.mk
@@ -3,7 +3,7 @@
 # libogg
 #
 #############################################################
-LIBOGG_VERSION = 1.3.0
+LIBOGG_VERSION = 1.3.1
 LIBOGG_SITE = http://downloads.xiph.org/releases/ogg
 LIBOGG_LICENSE = BSD-3c
 LIBOGG_LICENSE_FILES = COPYING
