21d9b8579e88db425cde1c6e0648eaa4554be683
Daniel Mack <zonque@gmail.com>
2013-10-18 17:35:07 +0200

    raumfeld/testsuite/rootfs/init.sh: explicitly load modules for now

--- raumfeld/testsuite/rootfs/init.sh
+++ raumfeld/testsuite/rootfs/init.sh
@@ -21,6 +21,14 @@ if [ -n "$(grep Geode /proc/cpuinfo)" ]; then
     modprobe ath5k
 elif [ -n "$(grep AM33XX /proc/cpuinfo)" ]; then
     # modules for ARMADA
+    modprobe snd-soc-adau1701
+    modprobe snd-soc-ak4104
+    modprobe snd-soc-ak5386
+    modprobe snd-soc-cs4271
+    modprobe snd-soc-sigmadsp
+    modprobe snd-soc-tas5086
+    modprobe snd-soc-davinci-mcasp
+    # FIXME: the above should actually be implicitly loaded by the next one
     modprobe snd-soc-s800
     modprobe mwifiex_sdio
 else
