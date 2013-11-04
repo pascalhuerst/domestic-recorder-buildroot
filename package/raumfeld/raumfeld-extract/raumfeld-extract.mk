fa1444ad6b04fb251e444825789b25ea1d2ed571
Sven Neumann <neumann@teufel.de>
2013-11-04 20:57:20 +0100

    raumfeld: add host-pkgconf as a dependency on raumfeld-extract

--- package/raumfeld/raumfeld-extract/raumfeld-extract.mk
+++ package/raumfeld/raumfeld-extract/raumfeld-extract.mk
@@ -6,6 +6,6 @@
 
 RAUMFELD_EXTRACT_MODULE = raumfeld-extract
 
-RAUMFELD_EXTRACT_DEPENDENCIES = libarchive
+RAUMFELD_EXTRACT_DEPENDENCIES = host-pkgconf libarchive
 
 $(eval $(raumfeld-autotools-package))
