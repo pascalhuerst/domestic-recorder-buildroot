8b5f494f5fa2c2cc3133b6841c2df47458d6596d
Sven Neumann <neumann@teufel.de>
2013-11-21 16:12:37 +0100

    gupnp-igd: update to version 0.2.2

--- package/gupnp-igd/gupnp-igd.mk
+++ package/gupnp-igd/gupnp-igd.mk
@@ -4,11 +4,12 @@
 #
 #############################################################
 
-GUPNP_IGD_VERSION = 0.1.6
-GUPNP_IGD_SOURCE = gupnp-igd-$(GUPNP_IGD_VERSION).tar.gz
-GUPNP_IGD_SITE = http://www.gupnp.org/sites/all/files/sources
+GUPNP_IGD_MAJOR_VERSION = 0.2
+GUPNP_IGD_MINOR_VERSION = 2
+GUPNP_IGD_VERSION = $(GUPNP_IGD_MAJOR_VERSION).$(GUPNP_IGD_MINOR_VERSION)
+GUPNP_IGD_SOURCE = gupnp-igd-$(GUPNP_IGD_VERSION).tar.xz
+GUPNP_IGD_SITE = http://ftp.gnome.org/pub/GNOME/sources/gupnp-igd/$(GUPNP_IGD_MAJOR_VERSION)
 GUPNP_IGD_INSTALL_STAGING = YES
-GUPNP_IGD_INSTALL_TARGET = YES
 
 GUPNP_IGD_CONF_ENV = \
 	ac_cv_path_GLIB_GENMARSHAL=$(LIBGLIB2_HOST_BINARY)
