5079b578551e1f5a4f1e63a2582adb4af459aa04
Sven Neumann <neumann@teufel.de>
2013-11-22 11:56:29 +0100

    iso-codes: fix gettext dependency
    Signed-off-by: Sven Neumann <neumann@teufel.de>

--- package/iso-codes/iso-codes.mk
+++ package/iso-codes/iso-codes.mk
@@ -8,6 +8,6 @@ ISO_CODES_VERSION = 3.16
 ISO_CODES_SOURCE = iso-codes-$(ISO_CODES_VERSION).tar.bz2
 ISO_CODES_SITE = ftp://pkg-isocodes.alioth.debian.org/pub/pkg-isocodes/
 ISO_CODES_INSTALL_STAGING = YES
-ISO_CODES_DEPENDENCIES = gettext
+ISO_CODES_DEPENDENCIES = $(if $(BR2_NEEDS_GETTEXT),gettext)
 
 $(eval $(autotools-package))
