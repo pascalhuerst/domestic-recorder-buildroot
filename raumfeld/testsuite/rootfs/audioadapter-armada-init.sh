2e4c125d32a4c5d4e9543596037e567d8c077290
Daniel Mack <zonque@gmail.com>
2014-02-26 10:39:37 +0100

    raumfeld: audioadapter-arm-init: revert to 18eabe8c7ba
    This reverts all changes to the original version. We want to keep
    armada-init around for the manual base I/O test, and switch over to
    armada-final once the test jig is deployed.

--- raumfeld/testsuite/rootfs/audioadapter-armada-init.sh
+++ raumfeld/testsuite/rootfs/audioadapter-armada-init.sh
@@ -15,39 +15,33 @@ kill_leds
 ./leds-blink-so 1 &
 ./armada-button
 
-kill_leds
-./leds-blink 4 &
-echo "Turn rotary encoder counter-clock-wise."
-$INPUT_TEST rotary_cw
+if [ -n "$(grep -i "Speaker L" /proc/device-tree/model)" ] ||
+   [ -n "$(grep -i "One" /proc/device-tree/model)" ]; then
 
-kill_leds
-./leds-blink 5 &
-echo "Turn rotary encoder clock-wise."
-$INPUT_TEST rotary_ccw
+	kill_leds
+	./leds-blink 4 &
+        echo "Turn rotary encoder counter-clock-wise."
+	$INPUT_TEST rotary_cw
+
+	kill_leds
+	./leds-blink 5 &
+        echo "Turn rotary encoder clock-wise."
+	$INPUT_TEST rotary_ccw
+fi
 
 kill_leds
 ./leds-blink-so 3 &
-echo "Testing ethernet..."
-./ethernet_armada
+./ethernet_armada 
 if [ $? -ne 0 ]; then
     kill_leds
     ./leds-blink-so 3 1 &
     exit 1
 fi
 
-if [ -n "$(grep -i "Test Jig" /proc/device-tree/model)" ]; then
-    kill_leds
-    led_on 1
-    led_off 2
-    echo "Testing pins..."
-    if ! $PINS_TEST; then
-        ./leds-blink-so 4 1 &
-        exit 1
-    fi
-fi
 
 echo "*** Updating u-boot *****"
 /update-uboot-armada.sh
+
 
 kill_leds
 
