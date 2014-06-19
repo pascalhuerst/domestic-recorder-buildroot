719072157b05e146cb9bd2f622a6d8ca476fb253
Sven Neumann <neumann@teufel.de>
2014-06-19 09:01:13 +0200

    raumfeld: add spotify-embedded dependency to renderer-ng

--- package/raumfeld/renderer-ng/renderer-ng.mk
+++ package/raumfeld/renderer-ng/renderer-ng.mk
@@ -4,6 +4,6 @@
 #
 #############################################################
 
-RENDERER_NG_DEPENDENCIES = host-pkgconf host-libglib2 alsa-lib flac libraumfelddsp
+RENDERER_NG_DEPENDENCIES = host-pkgconf host-libglib2 alsa-lib flac libraumfelddsp spotify-embedded
 
 $(eval $(raumfeld-cross-package))
