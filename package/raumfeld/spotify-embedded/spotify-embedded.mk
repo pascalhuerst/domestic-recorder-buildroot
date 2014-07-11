51004f20fae60f07ccbbbea726c7e4d2d4566f08
Sven Neumann <neumann@teufel.de>
2014-07-11 18:19:45 +0200

    spotify-embedded: adjust for v1.8.0-Raumfeld-PXA300-Release

--- package/raumfeld/spotify-embedded/spotify-embedded.mk
+++ package/raumfeld/spotify-embedded/spotify-embedded.mk
@@ -8,8 +8,8 @@ SPOTIFY_EMBEDDED_SITE = http://rf-devel.teufel.local/devel/builldroot/dl
 SPOTIFY_EMBEDDED_INSTALL_STAGING = YES
 
 ifeq ($(BR2_xscale),y)
-  SPOTIFY_EMBEDDED_VERSION = 1.6.0
-  SPOTIFY_EMBEDDED_SOURCE = spotify_embedded-v$(SPOTIFY_EMBEDDED_VERSION)-Linux-armv5-64bit-Release.tar.gz
+  SPOTIFY_EMBEDDED_VERSION = 1.8.0
+  SPOTIFY_EMBEDDED_SOURCE = spotify_embedded-v$(SPOTIFY_EMBEDDED_VERSION)-Raumfeld-PXA300-Release.tar.gz
 
 else ifeq ($(BR2_cortex_a8),y)
   SPOTIFY_EMBEDDED_VERSION = 1.8.2
