793595a6b2d4424c30ca1c79b4e01e79d768b303
Henry Hoegelow <henry@hoegelow.com>
2014-07-16 14:16:03 +0200

    raumfeld: removed rule to copy dsp files from renderer-ng to factory-tests as the factory-test for element (which is the only one that was actually copied) has an additional requirement of gaining the input signal by 3dB

--- package/raumfeld/factory-tests/factory-tests.mk
+++ package/raumfeld/factory-tests/factory-tests.mk
@@ -6,12 +6,10 @@
 
 FACTORY_TESTS_DEPENDENCIES = host-pkgconf alsa-lib libraumfelddsp renderer-ng-extract
 
-FACTORY_TESTS_MODELS = raumfeld-element
-
-define FACTORY_TESTS_INSTALL_DSP_CONFIG_FILES
-	$(foreach model,$(FACTORY_TESTS_MODELS),$(INSTALL) $(RENDERER_NG_SRCDIR)/dsp-config/$(model).xml $(TARGET_DIR)/raumfeld/factory-tests/dsp-config)
-endef
-
-FACTORY_TESTS_POST_INSTALL_TARGET_HOOKS += FACTORY_TESTS_INSTALL_DSP_CONFIG_FILES
+#FACTORY_TESTS_MODELS = raumfeld-element
+#define FACTORY_TESTS_INSTALL_DSP_CONFIG_FILES
+#	$(foreach model,$(FACTORY_TESTS_MODELS),$(INSTALL) $(RENDERER_NG_SRCDIR)/dsp-config/$(model).xml $(TARGET_DIR)/raumfeld/factory-tests/dsp-config)
+#endef
+#FACTORY_TESTS_POST_INSTALL_TARGET_HOOKS += FACTORY_TESTS_INSTALL_DSP_CONFIG_FILES
 
 $(eval $(raumfeld-cross-package))
