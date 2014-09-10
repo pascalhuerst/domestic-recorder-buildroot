61543f4a8fc79216cbbee4f3c3f52aaf87185ed0
Pascal Huerst <pascal.huerst@gmail.com>
2014-09-10 14:18:38 +0200

    raumfeld-dts: Prepared to compile dt using C Preprocessor followed by DTC, using includes in the linux tree

--- package/raumfeld/raumfeld-dts/raumfeld-dts.mk
+++ package/raumfeld/raumfeld-dts/raumfeld-dts.mk
@@ -13,7 +13,7 @@ RAUMFELD_DTS_DEPENDENCIES = linux host-cramfs
 define RAUMFELD_DTS_INSTALL_TARGET_CMDS
 	@(rm -rf ${BINARIES_DIR}/dts; \
 	  mkdir -p ${BINARIES_DIR}/dts; \
-	  make HOSTDIR=${HOST_DIR} DESTDIR=${BINARIES_DIR}/dts/ -C ${RAUMFELD_DTS_DIR})
+	  make LINUX_DIR=${LINUX_DIR} HOSTDIR=${HOST_DIR} DESTDIR=${BINARIES_DIR}/dts/ -C ${RAUMFELD_DTS_DIR})
 	@echo "Raumfeld device-tree blobs created in ${BINARIES_DIR}/dts"
 
 	@${HOST_DIR}/usr/bin/mkcramfs ${BINARIES_DIR}/dts ${BINARIES_DIR}/dts.cramfs
