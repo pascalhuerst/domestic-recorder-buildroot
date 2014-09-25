d8b2fe63f957f2df03ba9d8387bface05e9d309a
Sven Neumann <neumann@teufel.de>
2014-09-25 16:34:22 +0200

    raumfeld: remove renderer and loopbacktest packages
    These are not any longer built and will soon be removed from
    the core repository.

--- package/raumfeld/renderer/renderer.mk
+++
@@ -1,9 +0,0 @@
-#############################################################
-#
-# renderer
-#
-#############################################################
-
-RENDERER_DEPENDENCIES = host-pkgconf host-libglib2 alsa-lib flac libraumfeld
-
-$(eval $(raumfeld-cross-package))
