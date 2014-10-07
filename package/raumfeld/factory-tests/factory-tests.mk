ff4df536b73b403e9399913fa693c38a79417b82
Sven Neumann <neumann@teufel.de>
2014-10-07 21:41:15 +0200

    factory-tests: fix name of dsp-config file
    Fixup for RFS-5016

--- package/raumfeld/factory-tests/factory-tests.mk
+++ package/raumfeld/factory-tests/factory-tests.mk
@@ -6,7 +6,7 @@
 
 FACTORY_TESTS_DEPENDENCIES = host-pkgconf alsa-lib libraumfelddsp renderer-ng-extract
 
-FACTORY_TESTS_MODELS = raumfeld-element
+FACTORY_TESTS_MODELS = raumfeld-one-s
 
 define FACTORY_TESTS_INSTALL_DSP_CONFIG_FILES
 	$(foreach model,$(FACTORY_TESTS_MODELS),$(INSTALL) $(RENDERER_NG_SRCDIR)/dsp-config/$(model).xml $(TARGET_DIR)/raumfeld/factory-tests/dsp-config)
