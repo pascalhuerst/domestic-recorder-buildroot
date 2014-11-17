53b0693ac9a39675bd599bc31f4b04f3fcf9e487
Max Blagay <max.blagaj@gmail.com>
2014-11-17 17:28:58 +0100

    libasf: new package

---
+++ package/libasf/libasf.mk
@@ -0,0 +1,28 @@
+################################################################################
+#
+# libasf
+#
+################################################################################
+
+LIBASF_VERSION = 93152f139d3bcb3cf18dda3bdc513172002de5b4
+LIBASF_SITE = $(call github,juhovh,libasf,master,$(LIBASF_VERSION))
+LIBASF_LICENSE = LGPLv2.1+
+
+define LIBASF_CONFIGURE_CMDS
+        (cd $(@D); \
+                $(TARGET_CONFIGURE_OPTS)        \
+                $(MAKE1) clean \
+        )
+endef
+
+define LIBASF_BUILD_CMDS
+        (cd $(@D); $(TARGET_CONFIGURE_OPTS) $(MAKE) -j $(PARALLEL_JOBS))
+endef
+
+define LIBASF_INSTALL_TARGET_CMDS
+        (cd $(@D); $(TARGET_CONFIGURE_OPTS) $(MAKE) DESTDIR=$(TARGET_DIR) \
+                install)
+endef
+
+$(eval $(generic-package))
+
