011c62bea95db152467746fca3a9906fb80df204
Sven Neumann <neumann@teufel.de>
2014-11-18 16:39:16 +0100

    raumfeld: clean up postbuild scripts for legacy ARM and Geode
    As we have removed the end-of-line tests for these platforms,
    we do not any longer need to build these binaries.

--- raumfeld/postbuild-imgrootfs-geode.sh
+++ raumfeld/postbuild-imgrootfs-geode.sh
@@ -9,12 +9,4 @@ cp -r raumfeld/rootfs/etc/raumfeld-version $1/etc
 
 echo "Building and installing test binaries..."
 
-GCC=output/host/usr/bin/i586-linux-gcc
-
-$GCC -o $1/progress_fb -Wall raumfeld/testsuite/progress_fb/progress_fb.c
-$GCC -o $1/input_dump -Wall raumfeld/testsuite/input_dump/input_dump.c
-$GCC -o $1/input_test -Wall raumfeld/testsuite/input_test/input_test.c
-$GCC -o $1/percent -Wall raumfeld/testsuite/percent/percent.c
-$GCC -o $1/wireless_scan -Wall raumfeld/testsuite/wireless_scan/wireless_scan.c
-
 raumfeld/postbuild-cleanup.sh $*
