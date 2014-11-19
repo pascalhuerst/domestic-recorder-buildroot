69ad55949d6aacc61116f757ab646f97fbc03dbd
Max Blagay <max.blagaj@gmail.com>
2014-11-19 16:55:41 +0100

    Implemented makefiel for libasf

--- package/libasf/libasf.mk
+++ package/libasf/libasf.mk
@@ -7,20 +7,20 @@
 LIBASF_VERSION = 93152f139d3bcb3cf18dda3bdc513172002de5b4
 LIBASF_SITE = $(call github,juhovh,libasf,master,$(LIBASF_VERSION))
 LIBASF_LICENSE = LGPLv2.1+ 
-LIBASF_DEPENDENCIES = host-python
 
 define LIBASF_CONFIGURE_CMDS
         (cd $(@D); \
                 $(TARGET_CONFIGURE_OPTS)        \
-                $(HOST_DIR)/usr/bin/python2 ./waf configure --prefix=/usr )
+                $(MAKE1) clean \
+        )
 endef
 
 define LIBASF_BUILD_CMDS
-        (cd $(@D); $(HOST_DIR)/usr/bin/python2 ./waf build -j $(PARALLEL_JOBS))
+        (cd $(@D); $(TARGET_CONFIGURE_OPTS) $(MAKE) -j $(PARALLEL_JOBS))
 endef
 
 define LIBASF_INSTALL_TARGET_CMDS
-        (cd $(@D); $(HOST_DIR)/usr/bin/python2 ./waf --destdir=$(TARGET_DIR) \
+        (cd $(@D); $(TARGET_CONFIGURE_OPTS) $(MAKE) DESTDIR=$(TARGET_DIR) \
                 install)
 endef
 
