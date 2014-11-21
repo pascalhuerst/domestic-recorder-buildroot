b65a0085901717bc4d0dd6aad247731269cf842a
Max Blagay <max.blagaj@gmail.com>
2014-11-21 17:18:40 +0100

    libasf: improved fix for RFS-5189

--- package/libasf/libasf.mk
+++ package/libasf/libasf.mk
@@ -9,6 +9,12 @@ LIBASF_SITE = $(call github,juhovh,libasf,master,$(LIBASF_VERSION))
 LIBASF_INSTALL_STAGING = YES
 LIBASF_LICENSE = LGPLv2.1+
 
+define LIBASF_COPY_FILES
+        (cp package/libasf/Makefile package/libasf/libasf.pc.in $(@D)/)
+endef
+
+LIBASF_POST_EXTRACT_HOOKS += LIBASF_COPY_FILES
+
 define LIBASF_CONFIGURE_CMDS
         (cd $(@D); \
                 $(TARGET_CONFIGURE_OPTS)        \
@@ -17,12 +23,12 @@ define LIBASF_CONFIGURE_CMDS
 endef
 
 define LIBASF_BUILD_CMDS
-        (cd $(@D); $(TARGET_CONFIGURE_OPTS) $(MAKE) -j $(PARALLEL_JOBS))
+        (cd $(@D); $(TARGET_CONFIGURE_OPTS) $(MAKE))
 endef
 
 define LIBASF_INSTALL_STAGING_CMDS
-        (cd $(@D); $(TARGET_CONFIGURE_OPTS) $(MAKE) DESTDIR=$(STAGING_DIR) \
-                install)
+        (cd $(@D); \
+            $(TARGET_CONFIGURE_OPTS) $(MAKE) PACKAGE_DIR=/usr DESTDIR=$(STAGING_DIR) install)
 endef
 
 define LIBASF_INSTALL_TARGET_CMDS
