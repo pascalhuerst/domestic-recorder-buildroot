96e92a1cf514f066b2d311f99882e5361a25c253
Sven Neumann <neumann@teufel.de>
2014-11-25 10:08:56 +0100

    raumfeld: we still need input_test for the end-of-line tests

--- raumfeld/postbuild-imgrootfs-arm.sh
+++ raumfeld/postbuild-imgrootfs-arm.sh
@@ -13,6 +13,7 @@ echo "Building and installing test binaries..."
 
 GCC=output/host/usr/bin/arm-linux-gcc
 
+$GCC -o $1/input_test -Wall raumfeld/testsuite/input_test/input_test.c
 $GCC -o $1/progress_fb -Wall raumfeld/testsuite/progress_fb/progress_fb.c
 $GCC -o $1/percent -Wall raumfeld/testsuite/percent/percent.c
 
