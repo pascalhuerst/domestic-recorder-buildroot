8648ae2427e74c8533d397fac4cc2335241535d0
Sven Neumann <neumann@teufel.de>
2014-12-09 17:29:10 +0100

    renderer-ng: add dependency on kmod
    Fixup for RFS-5248

--- package/raumfeld/renderer-ng/renderer-ng.mk
+++ package/raumfeld/renderer-ng/renderer-ng.mk
@@ -4,6 +4,6 @@
 #
 #############################################################
 
-RENDERER_NG_DEPENDENCIES = host-pkgconf host-libglib2 alsa-lib flac libraumfelddsp spotify-embedded
+RENDERER_NG_DEPENDENCIES = host-pkgconf host-libglib2 alsa-lib flac kmod libraumfelddsp spotify-embedded
 
 $(eval $(raumfeld-cross-package))
