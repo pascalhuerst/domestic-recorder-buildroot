1510b3baae2b77d05ab573ba6a9460e780d72dbd
Normen Bolling <normen.bolling@teufel.de>
2015-01-06 17:35:33 +0100

    renderer: renamed renderer-ng -> renderer (2)
    RFS-5227

--- package/raumfeld/renderer/renderer.mk
+++ package/raumfeld/renderer/renderer.mk
@@ -4,6 +4,6 @@
 #
 #############################################################
 
-RENDERER_NG_DEPENDENCIES = host-pkgconf host-libglib2 alsa-lib flac kmod libraumfelddsp spotify-embedded
+RENDERER_DEPENDENCIES = host-pkgconf host-libglib2 alsa-lib flac kmod libraumfelddsp spotify-embedded
 
 $(eval $(raumfeld-cross-package))
