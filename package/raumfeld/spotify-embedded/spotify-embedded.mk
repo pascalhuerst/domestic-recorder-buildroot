ba825cade0f3f585fe98e27d8aa2909c8c24591c
Henry Hoegelow <henry@hoegelow.com>
2015-10-13 11:26:24 +0200

    raumfeld: added spotify lib with vorbis support, build from branch feature-spotify-mr

--- package/raumfeld/spotify-embedded/spotify-embedded.mk
+++ package/raumfeld/spotify-embedded/spotify-embedded.mk
@@ -4,21 +4,27 @@
 #
 #############################################################
 
-SPOTIFY_EMBEDDED_VERSION = 1.20.0
-SPOTIFY_EMBEDDED_SITE = http://rf-devel.teufel.local/devel/builldroot/dl
+SPOTIFY_EMBEDDED_VERSION = 1.20.10
+SPOTIFY_EMBEDDED_SITE = http://rf-devel.teufel.local/devel/buildroot/dl
 SPOTIFY_EMBEDDED_INSTALL_STAGING = YES
 
 ifeq ($(BR2_xscale),y)
   SPOTIFY_EMBEDDED_RELEASE = spotify_embedded-v$(SPOTIFY_EMBEDDED_VERSION)-Raumfeld-PXA300-Release
+  SPOTIFY_EMBEDDED_LIB = spotify_embedded-vorbis-v$(SPOTIFY_EMBEDDED_VERSION)-Raumfeld-PXA300-Release
 else ifeq ($(BR2_cortex_a8),y)
   SPOTIFY_EMBEDDED_RELEASE = spotify_embedded-v$(SPOTIFY_EMBEDDED_VERSION)-Raumfeld-AM33x-Release
+  SPOTIFY_EMBEDDED_LIB = spotify_embedded-vorbis-v$(SPOTIFY_EMBEDDED_VERSION)-Raumfeld-AM33x-Release
+else
+  SPOTIFY_EMBEDDED_RELEASE = spotify_embedded-v$(SPOTIFY_EMBEDDED_VERSION)-Linux-32bit-Release
+  SPOTIFY_EMBEDDED_LIB = spotify_embedded-vorbis-v$(SPOTIFY_EMBEDDED_VERSION)-Linux-32bit-Release
 endif
 
-SPOTIFY_EMBEDDED_SOURCE = $(SPOTIFY_EMBEDDED_RELEASE).tar.gz
+SPOTIFY_EMBEDDED_SOURCE = $(SPOTIFY_EMBEDDED_LIB).tar.gz
 SPOTIFY_EMBEDDED_SUBDIR = spotify_embedded/$(SPOTIFY_EMBEDDED_RELEASE)
 
 define SPOTIFY_EMBEDDED_INSTALL_STAGING_CMDS
 	install -D -m 644 $(@D)/$(SPOTIFY_EMBEDDED_SUBDIR)/include/spotify_embedded.h $(STAGING_DIR)/usr/include
+	install -D -m 644 $(@D)/$(SPOTIFY_EMBEDDED_SUBDIR)/include/spotify_embedded_compress.h $(STAGING_DIR)/usr/include
 	install -D -m 755 $(@D)/$(SPOTIFY_EMBEDDED_SUBDIR)/lib/libspotify_embedded_shared.so $(STAGING_DIR)/usr/lib
 endef
 
