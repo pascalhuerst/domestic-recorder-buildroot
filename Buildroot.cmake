5049b58f7e7ea05b0fc2ed55458518720b9aee30
Sam Thursfield <sam.thursfield@codethink.co.uk>
2015-10-14 10:50:51 +0200

    Add toplevel CMake file
    This wraps Buildroot and the Raumfeld image generation modules,
    replacing the build.sh and build-finish.sh shell scripts that were
    previously used.
    
    This is a *huge* amount more lines of code... the main benefit is
    that the dependencies between the different buildroot modules are
    taken into account, so far. This makes it easier to split things up
    further in future, and to implement some sort of artifact caching.
    
    Version number is set by passing -DRAUMFELD_VERSION=xx to CMake at
    configure-time.

---
+++ Buildroot.cmake
@@ -0,0 +1,286 @@
+#.rst:
+# Buildroot.cmake
+# ---------------
+#
+# Wraps the Buildroot build system inside a CMake build system.
+#
+# This may be useful if you have multiple Buildroot builds which depend on each
+# other, and you want to drive them from one place.
+#
+# It provides two commands: buildroot_target and buildroot_toolchain. See below
+# for documentation.
+#
+# This module only works when used in a CMakeLists.txt that is in the top
+# directory of a Buildroot source tree.
+
+if(("${CMAKE_SOURCE_DIR}" STREQUAL "${CMAKE_BINARY_DIR}") AND
+    ("${CMAKE_GENERATOR}" STREQUAL "Unix Makefiles"))
+    message(FATAL_ERROR "Please run CMake with a work directory that is not "
+                        "the top of the repo. Otherwise, the generated "
+                        "Makefile will overwrite Buildroot's Makefile.")
+endif()
+
+
+# ::
+#
+#     buildroot_target(<name>
+#                      CONFIG <buildroot_config_file>
+#                      OUTPUT <output_files>
+#                      [TOOLCHAIN <buildroot_target_name>])
+#
+# Creates a target called <name> which runs a Buildroot build of the
+# given config file.
+#
+# This function expects to be called from a CMakeLists.txt file that is in the
+# top level of a Buildroot repository.
+#
+# The build is run in a subdirectory of the current binary directory, named
+# "${name}". This means you can add multiple different buildroot targets.
+# It does not clean this directory before running the build, you should do this
+# yourself if you want a clean build -- there is a ${name}-clean target created
+# which you can use for that.
+#
+# The output of the 'make' command is written to a .log file in the build
+# directory, named for the target. This is a convenience for debugging
+# errors in the individual Buildroot builds.
+#
+# This expects `support/scripts/config` to be available. It's available as
+# scripts/config in the Linux source tree, but Buildroot doesn't seem to ship
+# it by default.
+#
+# If TOOLCHAIN is specified, the BR2_TOOLCHAIN_EXTERNAL config flag must be
+# enabled in buildroot_config_file, along with
+# BR2_TOOLCHAIN_EXTERNAL_PREINSTALLED=y and BR2_TOOLCHAIN_EXTERNAL_CUSTOM=y.
+# The BR2_TOOLCHAIN_PATH will be updated to point to the host/usr/bin directory
+# in the build output of buildroot_target_name.
+#
+# The target that is created does not get added to the ALL target, won't be
+# built by default. If you need that, do something like this (assuming you called
+# buildroot_target() with name'foo'):
+#
+#   add_custom_target(all-foo ALL DEPENDS foo)
+#
+function(buildroot_target name)
+    # The undocumented MAKE_TARGET option is used by buildroot_toolchain()
+    # internally.
+
+    set(one_value_keywords CONFIG TOOLCHAIN)
+    set(multi_value_keywords OUTPUT)
+    cmake_parse_arguments(BR "" "${one_value_keywords}" "${multi_value_keywords}" ${ARGN})
+
+    if(BR_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "Unparsed arguments to buildroot_target: ${BR_UNPARSED_ARGUMENTS}")
+    endif()
+
+    # Sets build_dir, buildroot_download_dir, config_commands
+    _buildroot_common_setup(${name})
+
+    set(output_files)
+    foreach(output ${BR_OUTPUT})
+        list(APPEND output_files ${build_dir}/${output})
+    endforeach()
+    list(GET output_files 0 first_output_file)
+
+    if(BR_TOOLCHAIN)
+        # External toolchain configuration
+        get_target_property(toolchain_path ${BR_TOOLCHAIN} BUILDROOT_HOST_TOOLS_PREFIX)
+        list(APPEND config_commands --set-str BR2_TOOLCHAIN_EXTERNAL_PATH ${toolchain_path})
+        set(toolchain_depends ${BR_TOOLCHAIN})
+    endif()
+
+    _buildroot_prepare_config(${build_dir} ${BR_CONFIG} ${config_commands})
+
+    _buildroot_make(all ${name} ${build_dir} "${output_files}" "" "${toolchain_depends}"
+                    "Building Buildroot config ${BR_CONFIG} to produce ${first_output_file}")
+
+    # Actual target
+    add_custom_target(${name}
+        DEPENDS ${output_files}
+        SOURCES ${BR_CONFIG}
+    )
+
+    set_target_properties(${name} PROPERTIES
+        BUILDROOT_BUILD_DIR
+            ${build_dir}
+        BUILDROOT_OUTPUT
+            ${first_output_file}
+        BUILDROOT_HOST_TOOLS_PREFIX
+            ${build_dir}/host/usr
+    )
+
+    _buildroot_clean_target(${name} ${build_dir})
+endfunction()
+
+# ::
+#
+#     buildroot_toolchain(<name>
+#                         CONFIG <buildroot_config_file)
+#
+# Creates a target called <name> which builds a Buildroot toolchain using the
+# given config file.
+#
+# See the generic buildroot_target() for more information on wrapping Buildroot
+# with CMake.
+#
+function(buildroot_toolchain name)
+    set(one_value_keywords CONFIG)
+    cmake_parse_arguments(BR_TOOLCHAIN "" "${one_value_keywords}" "" ${ARGN})
+
+    if(BR_TOOLCHAIN_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "Unparsed arguments to buildroot_toolchain: ${BR_TOOLCHAIN_UNPARSED_ARGUMENTS}")
+    endif()
+
+    # Sets build_dir, buildroot_download_dir, config_commands
+    _buildroot_common_setup(${name})
+
+    # Config preparation. This uses scripts/config from the Linux source tree.
+    _buildroot_prepare_config(${build_dir} ${BR_TOOLCHAIN_CONFIG} ${config_commands})
+
+    # Toolchain builds don't have a single output file that we can watch. It's
+    # actually important to not specify files from inside host/usr/ as outputs,
+    # because Make will delete those files if something goes wrong, but
+    # Buildroot doesn't track all the files it creates, so that file will then
+    # just be gone forever and you will be confused and your build will be
+    # broken.
+    #
+    # Instead, we run 'make toolchain' unconditionally, but we also create a
+    # tar.gz of the build output (anticipating the implementation of artifact
+    # sharing!), and the actual ${name} target depends on that artifact. This
+    # saves us rerunning 'make' all the time, which is good because 'make' in
+    # the Buildroot repo is pretty slow.
+    set(outputs "")
+    set(check_files ${build_dir}/host/usr/libexec/gcc)
+    set(extra_depends "")
+
+    _buildroot_make(toolchain ${name} ${build_dir} "${outputs}" "${check_files}" "${extra_depends}"
+                    "Building Buildroot toolchain from config ${BR_TOOLCHAIN_CONFIG}")
+
+    # Now create an artifact ...
+    set(toolchain_artifact ${CMAKE_CURRENT_BINARY_DIR}/${name}.tar.gz)
+    add_custom_command(
+        OUTPUT ${toolchain_artifact}
+        COMMAND
+            tar -c -z -f ${toolchain_artifact} .
+        WORKING_DIRECTORY
+            ${build_dir}/host
+        DEPENDS
+            ${CMAKE_CURRENT_BINARY_DIR}/${name}.stamp
+        VERBATIM
+    )
+
+    # Actual target
+    add_custom_target(${name}
+        DEPENDS ${toolchain_artifact}
+        SOURCES ${BR_TOOLCHAIN_CONFIG}
+    )
+
+    set_target_properties(${name} PROPERTIES
+        BUILDROOT_OUTPUT ${toolchain_artifact}
+        BUILDROOT_HOST_TOOLS_PREFIX ${build_dir}/host/usr
+    )
+
+    _buildroot_clean_target(${name} ${build_dir})
+endfunction()
+
+## ::
+##
+## Properties set by this module
+
+define_property(TARGET PROPERTY "BUILDROOT_BUILD_DIR"
+    BRIEF_DOCS "Path to the directory that contains all Buildroot build output for this build."
+    FULL_DOCS "x"
+    )
+
+define_property(TARGET PROPERTY "BUILDROOT_HOST_TOOLS_PREFIX"
+    BRIEF_DOCS "Path to host tools prefix (/usr) within the Buildroot build output"
+    FULL_DOCS "x"
+    )
+
+define_property(TARGET PROPERTY "BUILDROOT_OUTPUT"
+    BRIEF_DOCS "Path to the main output file product by this target."
+    FULL_DOCS "x"
+    )
+
+
+## ::
+##
+## Internal helper functions and macros.
+
+macro(_buildroot_common_setup target_name)
+    # Set some variables used in buildroot_target() and buildroot_toolchain().
+
+    set(build_dir ${CMAKE_CURRENT_BINARY_DIR}/${target_name})
+    file(MAKE_DIRECTORY ${build_dir})
+
+    # Buildroot downloads nearly 700MB of source code, it makes sense to share
+    # this between each of the builds. We use the BR2_DL_DIR setting to do that.
+    set(buildroot_download_dir ${CMAKE_CURRENT_BINARY_DIR}/dl)
+    file(MAKE_DIRECTORY ${buildroot_download_dir})
+
+    set(config_commands)
+
+    # It's possible to set BR2_DL_DIR in the environment, but the value
+    # from the .config file seems to override, which is a bit useless.
+    list(APPEND config_commands --set-str BR2_DL_DIR ${buildroot_download_dir})
+endmacro()
+
+function(_buildroot_prepare_config build_dir input commands)
+    # Config preparation. This uses scripts/config from the Linux source tree.
+    add_custom_command(
+        OUTPUT ${build_dir}/.config
+        COMMAND
+            cp ${input} ${build_dir}/.config
+        COMMAND
+            env CONFIG_=BR2_ support/scripts/config --file ${build_dir}/.config ${config_commands}
+        DEPENDS ${input}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        VERBATIM
+    )
+endfunction()
+
+function(_buildroot_make buildroot_target cmake_target_name build_dir outputs check_files extra_depends comment)
+    set(build_log ${CMAKE_CURRENT_BINARY_DIR}/${cmake_target_name}.log)
+
+    if(NOT outputs)
+        # If there's no specific output that we can look for, create a stamp.
+        set(stamp ${CMAKE_CURRENT_BINARY_DIR}/${cmake_target_name}.stamp)
+        set(stamp_command COMMAND date > ${stamp})
+        set(outputs ${stamp})
+    else()
+        set(stamp_command)
+    endif()
+
+    add_custom_command(
+        OUTPUT ${outputs}
+        # The `tee` is used in order that the individual build logs are easier
+        # to find. Without '-o pipefail', Make / Ninja would see the exit code
+        # `tee` rather than `make`, which would cause errors to be ignored.
+        COMMAND
+            set -o pipefail && make O=${build_dir} ${buildroot_target} | tee --append ${build_log}
+
+        ${stamp_command}
+
+        # Sanity check -- CMake itself doesn't seem to generate rules that would
+        # check if the command actually creates the output that it's meant to.
+        COMMAND
+            support/scripts/check-target-created-files ${name} ${outputs} ${check_files}
+
+        WORKING_DIRECTORY
+            ${CMAKE_CURRENT_SOURCE_DIR}
+        DEPENDS
+            ${build_dir}/.config ${extra_depends}
+        COMMENT ${comment}
+        VERBATIM
+    )
+endfunction()
+
+function(_buildroot_clean_target name build_dir)
+    add_custom_target(
+        ${name}-clean
+        COMMAND
+            echo FIXME: I'm not actually doing the clean!!! (just for development)
+            echo rm -Rf ${build_dir}
+        VERBATIM
+    )
+endfunction()
+
---
+++ Raumfeld.cmake
@@ -0,0 +1,218 @@
+set(RAUMFELD_VERSION_FILE raumfeld/rootfs/etc/raumfeld-version)
+
+set(RAUMFELD_VERSION)
+
+if(NOT RAUMFELD_VERSION)
+    message(WARNING "RAUMFELD_VERSION not defined, defaulting to version 0.0")
+    set(RAUMFELD_VERSION 0.0)
+endif()
+
+get_filename_component(RAUMFELD_VERSION_DIR ${RAUMFELD_VERSION_FILE} DIRECTORY)
+file(MAKE_DIRECTORY ${RAUMFELD_VERSION_DIR})
+
+file(WRITE ${RAUMFELD_VERSION_FILE} "${RAUMFELD_VERSION}\n")
+
+function(_rerun_cmake_if_file_is_modified filename)
+    # This is a hack but it's the only way to do it currently...
+    configure_file(
+        ${filename}
+        ${CMAKE_CURRENT_BINARY_DIR}/stamps/${filename}.stamp
+    )
+endfunction()
+
+# Ensure the configure stage reruns if the user modifies the version file,
+# so that the version embedded in the images always matches the filename.
+_rerun_cmake_if_file_is_modified(${RAUMFELD_VERSION_FILE})
+
+# ::
+#
+#    raumfeld_image_target(<filename>
+#                          TARGET_TYPE <type>
+#                          KERNEL <buildroot_target>
+#                          IMGROOTFS <buildroot_target>
+#                          ROOTFS <buildroot_target>
+#                          [DEVICE_TREE <buildroot_target>])
+#
+# Creates a single image suitable for flashing a Raumfeld device.
+#
+# The TARGET_TYPE keyword is a Raumfeld-specific keyword understood by the
+# raumfeld/imgcreate.sh program. See that program for more details.
+#
+# The KERNEL, IMGROOTFS and ROOTFS keywords should each name a target created
+# with the buildroot_target() command, that build a suitable uImage,
+# rootfs.ext2 and rootfs.tar.gz respectively.
+#
+# The DEVICE_TREE keyword is a bit special, because currently the raumfeld-dts
+# package is built within another Buildroot target. Whatever target you pass
+# needs to have the RAUMFELD_DEVICE_TREE_DIR target property set to point to
+# the directory that conatins dts.cramfs and dts/*.dtb.
+#
+# Example:
+#
+#   raumfeld_image_target(binaries/audioadapter-armada-flash.img
+#                         TARGET_TYPE audioadapter-armada-flash
+#                         KERNEL buildroot-initramfs-armada
+#                         IMGROOTFS buildroot-imgrootfs-armada
+#                         ROOTFS buildroot-audioadapter-armada
+#                         DEVICE_TREE buildroot-audioadapter-armada)
+#
+function(raumfeld_image_target filename)
+    set(one_value_keywords DEVICE_TREE KERNEL IMGROOTFS ROOTFS TARGET_TYPE)
+    cmake_parse_arguments(RAUMFELD "" "${one_value_keywords}" "" ${ARGN})
+
+    if(RAUMFELD_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "Unparsed arguments to raumfeld_image_target: ${RAUMFELD_UNPARSED_ARGUMENTS}")
+    endif()
+
+    if(NOT IS_ABSOLUTE ${filename})
+        set(filename ${CMAKE_CURRENT_BINARY_DIR}/${filename})
+    endif()
+
+    set(extra_args)
+
+    get_target_property(kernel_file ${RAUMFELD_KERNEL} BUILDROOT_OUTPUT)
+    get_target_property(imgrootfs_file ${RAUMFELD_IMGROOTFS} BUILDROOT_OUTPUT)
+    get_target_property(rootfs_file ${RAUMFELD_ROOTFS} BUILDROOT_OUTPUT)
+
+    if(RAUMFELD_DEVICE_TREE)
+        get_target_property(dts_directory ${RAUMFELD_DEVICE_TREE} RAUMFELD_DEVICE_TREE_DIR)
+        if(NOT dts_directory)
+            message(FATAL_ERROR "Target ${RAUMFELD_DEVICE_TREE} does not have RAUMFELD_DEVICE_TREE_DIR property set.")
+        endif()
+        list(APPEND extra_args --dts-dir=${dts_directory})
+    endif()
+
+    # This is a bit ugly -- we assume that the audioadapter/remotecontrol/base
+    # images contain genext2fs (i.e. BR2_HOST_GENEXT2FS is enabled in the
+    # config). Nicer than requiring it on the host, though, as it isn't
+    # generally packaged anywhere.
+    get_target_property(host_tools_prefix ${RAUMFELD_ROOTFS} BUILDROOT_HOST_TOOLS_PREFIX)
+    set(genext2fs ${host_tools_prefix}/bin/genext2fs)
+
+    file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/binaries)
+
+    add_custom_command(
+        OUTPUT
+            ${filename}
+        COMMAND
+            env GENEXT2FS=${genext2fs}
+            ${CMAKE_CURRENT_SOURCE_DIR}/raumfeld/imgcreate.sh
+                --output-file=${filename}
+                --target=${RAUMFELD_TARGET_TYPE}
+                --kernel=${kernel_file}
+                --base-rootfs-img=${imgrootfs_file}
+                --target-rootfs-tgz=${rootfs_file}
+                --download-dir=${CMAKE_CURRENT_BINARY_DIR}/dl
+                ${extra_args}
+        DEPENDS
+            ${RAUMFELD_KERNEL}
+            ${RAUMFELD_IMGROOTFS}
+            ${RAUMFELD_ROOTFS}
+            ${RAUMFELD_DEVICE_TREE}
+        WORKING_DIRECTORY
+            ${CMAKE_CURRENT_SOURCE_DIR}
+    )
+endfunction()
+
+# ::
+#
+#   raumfeld_updates_target(<filename>
+#                           TARGET_TYPE <directory>
+#                           HARDWARE_IDS <id> [<id> ...]
+#                           KERNEL <buildroot_target>
+#                           ROOTFS <buildroot_target>
+#                           [DEVICE_TREE <buildroot_target>])
+#
+# Create a tar archive of update images for a set of Raumfeld devices.
+#
+# The TARGET_TYPE keyword is a Raumfeld-specific keyword understood by the
+# raumfeld/updatecreate.sh program. See that program for more details.
+#
+# One Raumfeld target can be used in more than one Raumfeld device. An update
+# image and associated metadata file will be produced for each device ID listed
+# in HARDWARE_IDS. The list of IDs is maintained in raumfeld/updatecreate.sh
+# and (in core.git) libraumfeld/raumfeld/platform.h.
+#
+# The KERNEL, IMGROOTFS and ROOTFS keywords should each name a target created
+# with the buildroot_target() command, that build a suitable uImage,
+# rootfs.ext2 and rootfs.tar.gz respectively.
+#
+# The DEVICE_TREE keyword is a bit special, because currently the raumfeld-dts
+# package is built within another Buildroot target. Whatever target you pass
+# needs to have the RAUMFELD_DEVICE_TREE_DIR target property set to point to
+# the directory that conatins dts.cramfs and dts/*.dtb.
+#
+# Example:
+#
+#   raumfeld_update_target(
+#       binaries/updates-audioadapter-armada-${version}.tar
+#           TARGET_TYPE audioadapter-armada
+#           HARDWARE_IDS 9 10 11 12 13 14 16 17
+#
+#           KERNEL buildroot-initramfs-armada
+#           ROOTFS buildroot-audioadapter-armada
+#           DEVICE_TREE buildroot-audioadapter-armada
+#   )
+#
+# This is an example of the file listing inside the generated tar archive:
+#
+#   10.updates
+#   11.updates
+#   12.updates
+#   13.updates
+#   14.updates
+#   16.updates
+#   17.updates
+#   732c99f090468808d60d82a3229f3e504016c52bc5092468721953c7d740b5e5
+#   732c99f090468808d60d82a3229f3e504016c52bc5092468721953c7d740b5e5.sign
+#   9.updates
+function(raumfeld_updates_target filename)
+    set(one_value_keywords DEVICE_TREE KERNEL ROOTFS TARGET_TYPE)
+    set(multi_value_keywords HARDWARE_IDS)
+    cmake_parse_arguments(RAUMFELD "" "${one_value_keywords}" "${multi_value_keywords}" ${ARGN})
+
+    if(RAUMFELD_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "Unparsed arguments to raumfeld_updates_target: ${RAUMFELD_UNPARSED_ARGUMENTS}")
+    endif()
+
+    if(NOT RAUMFELD_KERNEL OR NOT RAUMFELD_ROOTFS)
+        message(FATAL_ERROR "Required arguments to raumfeld_updates_target not all set.")
+    endif()
+
+    if(NOT IS_ABSOLUTE ${filename})
+        set(filename ${CMAKE_CURRENT_BINARY_DIR}/${filename})
+    endif()
+
+    set(extra_args)
+
+    get_target_property(kernel_file ${RAUMFELD_KERNEL} BUILDROOT_OUTPUT)
+    get_target_property(rootfs_file ${RAUMFELD_ROOTFS} BUILDROOT_OUTPUT)
+
+    if(RAUMFELD_DEVICE_TREE)
+        get_target_property(dts_directory ${RAUMFELD_DEVICE_TREE} RAUMFELD_DEVICE_TREE_DIR)
+        if(NOT dts_directory)
+            message(FATAL_ERROR "Target ${RAUMFELD_DEVICE_TREE} does not have RAUMFELD_DEVICE_TREE_DIR property set.")
+        endif()
+        list(APPEND extra_args --dts-dir=${dts_directory})
+    endif()
+
+    string(REPLACE ";" "," hardware_ids_comma_list "${RAUMFELD_HARDWARE_IDS}")
+
+    add_custom_command(
+        OUTPUT
+            ${filename}
+        COMMAND
+            ${CMAKE_CURRENT_SOURCE_DIR}/raumfeld/updatecreate.sh
+                --output-file=${filename}
+                --hardware-ids=${hardware_ids_comma_list}
+                --target=${RAUMFELD_TARGET_TYPE}
+                --targz=${rootfs_file}
+                --kexec=${kernel_file}
+                ${extra_args}
+        DEPENDS
+            ${RAUMFELD_KERNEL}
+            ${RAUMFELD_ROOTFS}
+        WORKING_DIRECTORY
+            ${CMAKE_CURRENT_SOURCE_DIR}
+    )
+endfunction()
