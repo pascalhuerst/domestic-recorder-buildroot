4d25930ca796e8efc4a1e8b32ff41db8b6a924d1
Michael Drake <michael.drake@codethink.co.uk>
2015-12-10 11:24:16 +0000

    Make raumfeld-extract come from its own repo, rather than core.git.
    This means that the buildroot-initramfs-* artifacts no longer contain
    anything from core.git. If they did, we would need to rebuilt them on
    each commit to core.git, which would be wasteful, or we would need to
    figure out a way to build raumfeld-extract outside of buildroot and
    insert it into an already-generated uImage initramfs, which would be
    complicated.

--- package/raumfeld/raumfeld-extract/raumfeld-extract.mk
+++ package/raumfeld/raumfeld-extract/raumfeld-extract.mk
@@ -6,6 +6,32 @@
 
 RAUMFELD_EXTRACT_MODULE = raumfeld-extract
 
+RAUMFELD_EXTRACT_VERSION = $(call qstrip,$(BR2_PACKAGE_RAUMFELD_EXTRACT_VERSION))
+RAUMFELD_EXTRACT_SITE = $(call qstrip,$(BR2_PACKAGE_RAUMFELD_EXTRACT_REPOSITORY))
+RAUMFELD_EXTRACT_SITE_METHOD = git
+
 RAUMFELD_EXTRACT_DEPENDENCIES = libarchive
 
-$(eval $(raumfeld-dummy-package))
+define inner-raumfeld-cmake-package
+
+ifndef $(2)_MODULE
+  $(2)_MODULE = $(1)
+endif
+
+
+# We use CMAKE_SYSTEM_PROCESSOR in libraumfeld/CMakeLists to work out which
+# variant of libunwind to link to. Buildroot should probably define this in
+# the toolchain file that pkg-cmake.mk generates, it doesn't at the moment.
+$(2)_CONF_OPTS += -DCMAKE_SYSTEM_PROCESSOR=$(KERNEL_ARCH)
+
+$(2)_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE="" -DCMAKE_C_FLAGS_RELEASE=""
+
+$(2)_CONF_OPTS += -DIN_BUILDROOT_CONTEXT=1
+
+# Call the generic cmake package infrastructure to generate the necessary
+# make targets
+$(call inner-cmake-package,$(1),$(2),$(2),target)
+
+endef # inner-raumfeld-cmake-package
+
+$(eval $(inner-raumfeld-cmake-package))
