060475879c052cc44ece875c52b358b39c2f54dc
Tim Keszler <tim.keszler@teufel.de>
2016-02-02 18:02:25 +0100

    spotify-embedded: use version from RAD-990

--- package/raumfeld/spotify-embedded/spotify-embedded.mk
+++ package/raumfeld/spotify-embedded/spotify-embedded.mk
@@ -14,12 +14,12 @@ ifeq ($(BR2_xscale),y)
 else ifeq ($(BR2_cortex_a8),y)
   SPOTIFY_EMBEDDED_RELEASE = spotify_embedded-v$(SPOTIFY_EMBEDDED_VERSION)-Raumfeld-AM33x-Release
   SPOTIFY_EMBEDDED_LIB = spotify_embedded-vorbis-v$(SPOTIFY_EMBEDDED_VERSION)-Raumfeld-AM33x-Release
+else ifeq ($(BR2_i386),y)
+  SPOTIFY_EMBEDDED_RELEASE = spotify_embedded-v$(SPOTIFY_EMBEDDED_VERSION)-Linux-32bit-Release
+  SPOTIFY_EMBEDDED_LIB = spotify_embedded-vorbis-v$(SPOTIFY_EMBEDDED_VERSION)-Linux-32bit-Release
 else ifeq ($(BR2_x86_64),y)
   SPOTIFY_EMBEDDED_RELEASE = spotify_embedded-v$(SPOTIFY_EMBEDDED_VERSION)-Linux-64bit-Release
   SPOTIFY_EMBEDDED_LIB = spotify_embedded-vorbis-v$(SPOTIFY_EMBEDDED_VERSION)-Linux-64bit-Release
-else
-  SPOTIFY_EMBEDDED_RELEASE = spotify_embedded-v$(SPOTIFY_EMBEDDED_VERSION)-Linux-32bit-Release
-  SPOTIFY_EMBEDDED_LIB = spotify_embedded-vorbis-v$(SPOTIFY_EMBEDDED_VERSION)-Linux-32bit-Release
 endif
 
 SPOTIFY_EMBEDDED_SOURCE = $(SPOTIFY_EMBEDDED_LIB).tar.gz
@@ -27,7 +27,7 @@ SPOTIFY_EMBEDDED_SUBDIR = spotify_embedded/$(SPOTIFY_EMBEDDED_RELEASE)
 
 define SPOTIFY_EMBEDDED_INSTALL_STAGING_CMDS
 	install -D -m 644 $(@D)/$(SPOTIFY_EMBEDDED_SUBDIR)/include/spotify_embedded.h $(STAGING_DIR)/usr/include
-	install -D -m 644 $(@D)/$(SPOTIFY_EMBEDDED_SUBDIR)/include/spotify_embedded_compress.h $(STAGING_DIR)/usr/include
+  install -D -m 644 $(@D)/$(SPOTIFY_EMBEDDED_SUBDIR)/include/spotify_embedded_compress.h $(STAGING_DIR)/usr/include
 	install -D -m 755 $(@D)/$(SPOTIFY_EMBEDDED_SUBDIR)/lib/libspotify_embedded_shared.so $(STAGING_DIR)/usr/lib
 endef
 
