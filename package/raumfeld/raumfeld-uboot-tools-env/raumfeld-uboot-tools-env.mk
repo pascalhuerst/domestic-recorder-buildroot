3d40765ec64da510a6f814fdd4c7018bf9994482
Vishal Thanki <vishal.thanki@teufel.de>
2016-07-14 17:19:25 +0200

    raumfeld: Add new package raumfeld-uboot-tools-env
    This package, if selected, installs the configuration file
    required for uboot tools to function correctly.
    
    RKL-35
    
    Signed-off-by: Vishal Thanki <vishal.thanki@teufel.de>

---
+++ package/raumfeld/raumfeld-uboot-tools-env/raumfeld-uboot-tools-env.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# raumfeld-uboot-tools-env
+#
+################################################################################
+
+RAUMFELD_UBOOT_TOOLS_ENV_SOURCE =
+RAUMFELD_UBOOT_TOOLS_ENV_FW_CONFIG = fw_env.config
+RAUMFELD_UBOOT_TOOLS_ENV_ARCH = $(shell echo $(BR2_CONFIG) | tr "\/" "\n" | tail -n 2 | head -n 1 | cut -d'-' -f3)
+
+ifeq ($(RAUMFELD_UBOOT_TOOLS_ENV_ARCH),armada)
+	RAUMFELD_UBOOT_TOOLS_ENV_SRC_FW_CONFIG = armada_fw_env.config
+else ifeq ($(RAUMFELD_UBOOT_TOOLS_ENV_ARCH),arm)
+	RAUMFELD_UBOOT_TOOLS_ENV_SRC_FW_CONFIG = arm_fw_env.config
+endif
+
+ifneq ($(RAUMFELD_UBOOT_TOOLS_ENV_SRC_FW_CONFIG),)
+define RAUMFELD_UBOOT_TOOLS_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0644 -D package/raumfeld/raumfeld-uboot-tools/$(RAUMFELD_UBOOT_TOOLS_ENV_SRC_FW_CONFIG) \
+		$(TARGET_DIR)/etc/$(RAUMFELD_UBOOT_TOOLS_ENV_FW_CONFIG)
+endef
+endif
+
+$(eval $(generic-package))
