b0ba140303a141f203846e4220279d31bff3221c
Vishal Thanki <vishal.thanki@teufel.de>
2016-10-19 16:12:49 +0200

    raumfeld: imx7: Add fw_env.config file for imx7
    RKL-70
    
    Signed-off-by: Vishal Thanki <vishal.thanki@teufel.de>

--- package/raumfeld/raumfeld-uboot-tools-env/raumfeld-uboot-tools-env.mk
+++ package/raumfeld/raumfeld-uboot-tools-env/raumfeld-uboot-tools-env.mk
@@ -8,7 +8,9 @@ RAUMFELD_UBOOT_TOOLS_ENV_SOURCE =
 RAUMFELD_UBOOT_TOOLS_ENV_FW_CONFIG = fw_env.config
 RAUMFELD_UBOOT_TOOLS_ENV_ARCH = $(shell echo $(BR2_CONFIG) | tr "\/" "\n" | tail -n 2 | head -n 1 | cut -d'-' -f3)
 
-ifeq ($(RAUMFELD_UBOOT_TOOLS_ENV_ARCH),armada)
+ifeq ($(RAUMFELD_UBOOT_TOOLS_ENV_ARCH),imx7)
+	RAUMFELD_UBOOT_TOOLS_ENV_SRC_FW_CONFIG = imx7_fw_env.config
+else ifeq ($(RAUMFELD_UBOOT_TOOLS_ENV_ARCH),armada)
 	RAUMFELD_UBOOT_TOOLS_ENV_SRC_FW_CONFIG = armada_fw_env.config
 else ifeq ($(RAUMFELD_UBOOT_TOOLS_ENV_ARCH),arm)
 	RAUMFELD_UBOOT_TOOLS_ENV_SRC_FW_CONFIG = arm_fw_env.config
