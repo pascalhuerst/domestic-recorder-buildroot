6a22bcf4c6f0bd7302b79c3bc9f48d41f345e9bc
Sven Neumann <neumann@teufel.de>
2016-12-08 15:38:42 +0100

    gc4a: remove unused gc4a helper scripts

--- gc4a/install/accept-tos.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-dbus-send --system --dest=com.streamunlimited.StreamCastDaemon1 --print-reply /com/streamunlimited/StreamCastDaemon1 com.streamunlimited.StreamCastDaemon1.AcceptTos
--- gc4a/install/create-cache.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir -p /tmp/chrome-cache
-chown root:chrome /tmp/chrome-cache
-chmod 775 /tmp/chrome-cache
-chmod 1777 /dev/shm
