074ad53844c9be0b7bb01e71bc85866dfaabc31c
Vishal Thanki <vishal.thanki@teufel.de>
2017-02-09 16:27:50 +0100

    imx7: Add uboot environment config file for constant partition
    While at it, refactored the way config files are copied to rootfs
    for other platforms as well.
    
    RKL-104
    
    Signed-off-by: Vishal Thanki <vishal.thanki@teufel.de>

--- package/raumfeld/raumfeld-uboot-tools-env/raumfeld-uboot-tools-env.mk
+++ package/raumfeld/raumfeld-uboot-tools-env/raumfeld-uboot-tools-env.mk
@@ -5,22 +5,10 @@
 ################################################################################
 
 RAUMFELD_UBOOT_TOOLS_ENV_SOURCE =
-RAUMFELD_UBOOT_TOOLS_ENV_FW_CONFIG = fw_env.config
 RAUMFELD_UBOOT_TOOLS_ENV_ARCH = $(shell echo $(BR2_CONFIG) | tr "\/" "\n" | tail -n 2 | head -n 1 | cut -d'-' -f3)
 
-ifeq ($(RAUMFELD_UBOOT_TOOLS_ENV_ARCH),imx7)
-	RAUMFELD_UBOOT_TOOLS_ENV_SRC_FW_CONFIG = imx7_fw_env.config
-else ifeq ($(RAUMFELD_UBOOT_TOOLS_ENV_ARCH),armada)
-	RAUMFELD_UBOOT_TOOLS_ENV_SRC_FW_CONFIG = armada_fw_env.config
-else ifeq ($(RAUMFELD_UBOOT_TOOLS_ENV_ARCH),arm)
-	RAUMFELD_UBOOT_TOOLS_ENV_SRC_FW_CONFIG = arm_fw_env.config
-endif
-
-ifneq ($(RAUMFELD_UBOOT_TOOLS_ENV_SRC_FW_CONFIG),)
 define RAUMFELD_UBOOT_TOOLS_ENV_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0644 -D package/raumfeld/raumfeld-uboot-tools-env/$(RAUMFELD_UBOOT_TOOLS_ENV_SRC_FW_CONFIG) \
-		$(TARGET_DIR)/etc/$(RAUMFELD_UBOOT_TOOLS_ENV_FW_CONFIG)
+	$(INSTALL) -m 0644 -D package/raumfeld/raumfeld-uboot-tools-env/$(RAUMFELD_UBOOT_TOOLS_ENV_ARCH)/* $(TARGET_DIR)/etc/
 endef
-endif
 
 $(eval $(generic-package))
